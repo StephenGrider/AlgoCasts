11ac28ee8a6444090ec6258873cb04365107603e
stephengrider <ste.grider@gmail.com>
2017-10-30 11:00:49 -0700

    Added descriptions

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -1,3 +1,10 @@
+// --- Directions
+// Write a program that prints the numbers
+// from 1 to 100. But for multiples of three print
+// “Fizz” instead of the number and for the multiples
+// of five print “Buzz”. For numbers which are multiples
+// of both three and five print “FizzBuzz”.
+
 function fizzBuzz(n) {}
 
 module.exports = fizzBuzz;
