4443165061361eb96f114abf6928d24fcac0194c
stephengrider <ste.grider@gmail.com>
2017-11-02 16:00:52 -0700

    steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,34 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  const add = stair.length <= row ? '#' : ' ';
+  steps(n, row, stair + add);
+}
 
 module.exports = steps;
+
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+//
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//
+//     console.log(stair);
+//   }
+// }
