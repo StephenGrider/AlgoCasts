b28f59d5acfb7f74cd68b5a3ab8b3133df929c3e
stephengrider <ste.grider@gmail.com>
2017-11-03 15:37:59 -0700

    queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,18 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
 
 module.exports = Queue;
