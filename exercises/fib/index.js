d23106a6316d317dd7a6e85ab338abc52da95efe
stephengrider <ste.grider@gmail.com>
2017-11-03 14:53:22 -0700

    fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,41 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
+
+// function fib(n) {
+//   const result = [0, 1];
+//
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+//
+//     result.push(a + b);
+//   }
+//
+//   return result[n];
+// }
