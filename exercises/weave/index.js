ded01913307fe6a3c52e6e3b364fa14438381a0d
stephengrider <ste.grider@gmail.com>
2017-11-03 16:34:34 -0700

    weave

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -4,7 +4,9 @@
 // receives two queues as arguments and combines the
 // contents of each into a new, third queue.
 // The third queue should contain the *alterating* content
-// of the two queues
+// of the two queues.  The function should handle
+// queues of different lengths without inserting
+// 'undefined' into the new one.
 // *Do not* access the array inside of any queue, only
 // use the 'add', 'remove', and 'peek' functions.
 // --- Example
