a44fa40e4df2dd1fbf7acbbde588b4e1ed36548d
stephengrider <ste.grider@gmail.com>
2017-11-03 15:30:51 -0700

    queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -2,7 +2,7 @@
 // Create a queue data structure.  The queue
 // should be a class with methods 'add' and 'remove'.
 // Adding to the queue should store an element until
-// it is removed;
+// it is removed
 // --- Examples
 //     const q = new Queue();
 //     q.add(1);
