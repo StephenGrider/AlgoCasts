596bbfd81611c3a7796a0bd74e3b3d33ae511727
stephengrider <ste.grider@gmail.com>
2017-11-06 20:42:46 -0800

    get last

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -33,6 +33,20 @@ class LinkedList {
   getFirst() {
     return this.head;
   }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
 }
 
 module.exports = { Node, LinkedList };
