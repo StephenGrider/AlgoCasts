b0bdda388f478f47718a065c290883601b0c4a17
stephengrider <ste.grider@gmail.com>
2017-11-08 15:45:18 -0800

    fromlast

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -1,7 +1,7 @@
 // --- Directions
-// Given a linked list, return the element n spaces
-// from the last node in the list.  Do not call the 'size'
-// method of the linked list.  Assume that n will always
+// Given a linked list and integer n, return the element n
+// spaces from the last node in the list.  Do not call the
+// 'size' method of the linked list.  Assume that n will always
 // be less than the length of the list.
 // --- Examples
 //    const list = new List();
