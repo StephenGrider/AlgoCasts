0c189a7c7b9888f828119460251806f40e8250f3
stephengrider <ste.grider@gmail.com>
2017-11-08 14:34:18 -0800

    midpoint

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -12,6 +12,16 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+function midpoint(list) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+}
 
 module.exports = midpoint;
