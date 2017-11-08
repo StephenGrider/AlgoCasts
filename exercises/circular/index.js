7fb0bb4be930a4dbce21358df23cc57dae23c520
stephengrider <ste.grider@gmail.com>
2017-11-08 15:02:56 -0800

    circular

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,20 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 module.exports = circular;
