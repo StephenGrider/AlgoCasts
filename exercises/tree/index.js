fe31129a87596797ebb7bfd596eb87b01376cfd5
stephengrider <ste.grider@gmail.com>
2017-11-08 17:07:24 -0800

    tree

---
+++ exercises/tree/index.js
@@ -0,0 +1,16 @@
+// --- Directions
+// 1) Create a node class.  The constructor
+// should accept an argument that gets assigned
+// to the data property and initialize an
+// empty array for storing children. The node
+// class should have methods 'add' and 'remove'.
+// 2) Create a tree class. The tree constructor
+// should initialize a 'root' property to null.
+// 3) Implement 'traverseBFS' and 'traverseDFS'
+// on the tree class
+
+class Node {}
+
+class Tree {}
+
+module.exports = { Tree, Node };
