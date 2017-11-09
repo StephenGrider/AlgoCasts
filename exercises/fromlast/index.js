3aeb24baf9a53509e0a362acea92778ba1b104f9
stephengrider <ste.grider@gmail.com>
2017-11-08 16:01:12 -0800

    from last

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,21 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  while (n > 0) {
+    fast = fast.next;
+    n--;
+  }
+
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow;
+}
 
 module.exports = fromLast;
