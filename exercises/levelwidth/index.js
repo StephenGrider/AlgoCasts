39521ead506533d403cf48aa16dfe04dab676b5b
stephengrider <ste.grider@gmail.com>
2017-11-10 12:33:05 -0800

    levelwidth solution

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,23 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const arr = [root, 's'];
+  const counters = [0];
+
+  while (arr.length > 1) {
+    const node = arr.shift();
+
+    if (node === 's') {
+      counters.push(0);
+      arr.push('s');
+    } else {
+      arr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+
+  return counters;
+}
 
 module.exports = levelWidth;
