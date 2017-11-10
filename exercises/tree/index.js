37cf6538d986b5bb6a5d9e794faf02a639fde233
stephengrider <ste.grider@gmail.com>
2017-11-10 11:39:43 -0800

    tree

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -31,6 +31,26 @@ class Tree {
   constructor() {
     this.root = null;
   }
+
+  traverseBF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
+
+  traverseDF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  }
 }
 
 module.exports = { Tree, Node };
