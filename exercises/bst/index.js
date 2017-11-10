35ac131419979f87464caea5c8d2abdfa2b398ff
stephengrider <ste.grider@gmail.com>
2017-11-10 14:16:56 -0800

    bst

---
+++ exercises/bst/index.js
@@ -0,0 +1,13 @@
+// --- Directions
+// 1) Implement the Node class to create
+// a binary search tree.  The constructor
+// should initialize values 'data', 'left',
+// and 'right'.
+// 2) Implement the 'insert' method for the
+// Node class.  Insert should accept an argument
+// 'data', then create an insert a new node
+// at the appropriate location in the tree.
+
+class Node {}
+
+module.exports = Node;
