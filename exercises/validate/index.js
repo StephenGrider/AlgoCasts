f86fca808f1f723416a9d3e07cca7e6f33e23d77
stephengrider <ste.grider@gmail.com>
2017-11-10 16:25:52 -0800

    validate

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,24 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node) {}
+function validate(node, min = null, max = null) {
+  if (max !== null && node.data > max) {
+    return false;
+  }
+
+  if (min !== null && node.data < min) {
+    return false;
+  }
+
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
+  }
+
+  return true;
+}
 
 module.exports = validate;
