cab00ec604b015da7deee981e93755aaf26b121e
stephengrider <ste.grider@gmail.com>
2017-11-13 11:22:55 -0800

    bst

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -7,6 +7,9 @@
 // Node class.  Insert should accept an argument
 // 'data', then create an insert a new node
 // at the appropriate location in the tree.
+// 3) Implement the 'contains' method for the Node
+// class.  Contains should accept a 'data' argument
+// and return the Node in the tree with the same value.
 
 class Node {
   constructor(data) {
--- exercises/bst/test.js
+++ exercises/bst/test.js
@@ -14,3 +14,28 @@ test('Node can insert correctly', () => {
   expect(node.right.data).toEqual(15);
   expect(node.right.right.data).toEqual(17);
 });
+
+test('Contains returns node with the same data', () => {
+  const node = new Node(10);
+  node.insert(5);
+  node.insert(15);
+  node.insert(20);
+  node.insert(0);
+  node.insert(-5);
+  node.insert(3);
+
+  const three = node.left.left.right;
+  expect(node.contains(3)).toEqual(three);
+});
+
+test('Contains returns null if value not found', () => {
+  const node = new Node(10);
+  node.insert(5);
+  node.insert(15);
+  node.insert(20);
+  node.insert(0);
+  node.insert(-5);
+  node.insert(3);
+
+  expect(node.contains(9999)).toEqual(null);
+});
