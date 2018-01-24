92337190db7ea79f934fb309510fc5027f5450d1
James McCormack <thejamesmccormack@gmail.com>
2018-01-24 09:14:20 -0800

    three solutions to first algorithm

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,30 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// this versions uses reduce, which takes the empty string and passes it into the callback as the first argument.
+
+function reverse(str) {
+  return str.split('').reduce((reversed, character) => character + reversed, '')
+}
+
+// this is a perfectly acceptable answer. it uses the new ES6 "for - of" syntax instead of a standard for-loop.
+
+// function reverse(str) {
+//   let reversed = '';
+//   for (let character of str) {
+//     reversed = character + reversed
+//   }
+//   return reversed
+// }
+
+
+
+// this is the 'cheater' method and won't be allowed as an answer on most interviews!
+
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+
 
 module.exports = reverse;
