46cb8ff39b9066655ceabfcae85a32e5edd8e9ed
Preet Dha <manpreetdha+github@gmail.com>
2018-04-14 16:34:10 -0700

    Use a third way to detect a palindrome using the JS array function every()

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -25,3 +25,9 @@ function palindrome(str) {
   }
   return reversedString === str;
 }
+
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
+}
