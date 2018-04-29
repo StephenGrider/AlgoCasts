201ceba498453395713f8c53a9f5a2764f44c7e3
smithwebtek <brad@smithwebtek.com>
2018-04-29 18:55:21 -0400

    Do exercises

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,8 +6,53 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+
 function reverse(str) {
-	return str.split('').reverse().join('')
+	let newStr = ''
+	for (let char of str) {
+		debugger
+		newStr = char + newStr
+	}
+	return newStr;
 }
+
+reverse('bradley')
+
+// 1
+// function reverse(str) {
+// 	return str.split('').reverse().join('')
+// }
+
+// 2
+// function reverse(str) {
+// 	let newStr = ''
+// 	for (let char of str) {
+// 		newStr = char + newStr
+// 	}
+// 	return newStr;
+// }
+
+// 3
+// function reverse(str) {
+// 	return str.split('').reduce((reversed, character) => {
+// 		return reversed = character + reversed
+// 	}, '')
+// }
+
+// 4
+// function reverse(str) {
+// 	return str.split('').reduce((reversed, character) => reversed = character + reversed, '')
+// }
+
+// 5
+// function reverse(str) {
+// 	return str.split('').reduce((acc, cv) => acc = cv + acc, '')
+// }
+
+// 6
+// function reverse(str) {
+// 	return str.split('').reduce((acc, cv) => acc = cv + acc, '')
+// }
+
 
 module.exports = reverse;
