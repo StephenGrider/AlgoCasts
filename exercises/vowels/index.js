ead2b175d3b728ca447c9ba8749a0601809e8310
smithwebtek <brad@smithwebtek.com>
2018-05-04 19:15:37 -0400

    Solve vowels with Regex

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,16 +7,40 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
+
+
+
+//================== regex ==================
 function vowels(str) {
-	const vowels = ['a', 'e', 'i', 'o', 'u']
-	let tempStr = str.split('')
-	let vowelCount = 0
-	tempStr.forEach(letter => {
-		if (vowels.includes(letter.toLowerCase())) {
-			vowelCount++
-		}
-	})
-	return vowelCount;
+	const matches = str.match(/[aeiou]/gi)
+	return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
+
+// ================ iterative ======================
+// function vowels(str) {
+// 	const vowels = ['a', 'e', 'i', 'o', 'u']
+// 	let tempStr = str.split('')
+// 	let count = 0
+// 	tempStr.forEach(letter => {
+// 		if (vowels.includes(letter.toLowerCase())) {
+// 			count++
+// 		}
+// 	})
+// 	return count;
+// }
+
+// ================ iterative 1 ======================
+// function vowels(str) {
+	// 	let count = 0;
+	// 	const v = ['a', 'e', 'i', 'o', 'u']
+// 	const b = 'aeiou'
+
+// 	for (let char of str.toLowerCase()) {
+// 		if (b.includes(char)) {
+// 			count++
+// 		}
+// 	}
+// 	return count
+// }
