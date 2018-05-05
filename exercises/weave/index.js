4218bafad32df3e7be4170219f7e68e698aa068f
Danielle Gellis <danisyellis@gmail.com>
2018-05-05 14:29:46 -0700

    Fixes a small typo- "alterating" to "alternating"

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -3,7 +3,7 @@
 // 2) Implement the 'weave' function.  Weave
 // receives two queues as arguments and combines the
 // contents of each into a new, third queue.
-// The third queue should contain the *alterating* content
+// The third queue should contain the *alternating* content
 // of the two queues.  The function should handle
 // queues of different lengths without inserting
 // 'undefined' into the new one.
