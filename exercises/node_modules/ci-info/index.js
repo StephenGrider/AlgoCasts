486952c0c6a874618ea2e1ff17fadba89331c01e
ryanscottjohnson <teacherman1@gmail.com>
2018-08-16 14:03:45 -0700

    initial commit

---
+++ exercises/node_modules/ci-info/index.js
@@ -0,0 +1,51 @@
+'use strict'
+
+var env = process.env
+
+var vendors = [
+  // Constant, Name, Envs
+  ['APPVEYOR', 'AppVeyor', 'APPVEYOR'],
+  ['BAMBOO', 'Bamboo', 'bamboo_planKey'],
+  ['BITBUCKET', 'Bitbucket Pipelines', 'BITBUCKET_COMMIT'],
+  ['BUILDKITE', 'Buildkite', 'BUILDKITE'],
+  ['CIRCLE', 'CircleCI', 'CIRCLECI'],
+  ['CODEBUILD', 'AWS CodeBuild', 'CODEBUILD_BUILD_ARN'],
+  ['CODESHIP', 'Codeship', {CI_NAME: 'codeship'}],
+  ['DRONE', 'Drone', 'DRONE'],
+  ['GITLAB', 'GitLab CI', 'GITLAB_CI'],
+  ['GOCD', 'GoCD', 'GO_PIPELINE_LABEL'],
+  ['HUDSON', 'Hudson', 'HUDSON_URL'],
+  ['JENKINS', 'Jenkins', 'JENKINS_URL', 'BUILD_ID'],
+  ['MAGNUM', 'Magnum CI', 'MAGNUM'],
+  ['SEMAPHORE', 'Semaphore', 'SEMAPHORE'],
+  ['SOLANO', 'Solano CI', 'TDDIUM'],
+  ['STRIDER', 'Strider CD', 'STRIDER'],
+  ['TASKCLUSTER', 'TaskCluster', 'TASK_ID', 'RUN_ID'],
+  ['TDDIUM', 'Solano CI', 'TDDIUM'], // Deprecated
+  ['TEAMCITY', 'TeamCity', 'TEAMCITY_VERSION'],
+  ['TFS', 'Team Foundation Server', 'TF_BUILD'],
+  ['TRAVIS', 'Travis CI', 'TRAVIS']
+]
+
+exports.name = null
+
+vendors.forEach(function (vendor) {
+  var constant = vendor.shift()
+  var name = vendor.shift()
+  var isCI = vendor.every(function (obj) {
+    if (typeof obj === 'string') return !!env[obj]
+    return Object.keys(obj).every(function (k) {
+      return env[k] === obj[k]
+    })
+  })
+  exports[constant] = isCI
+  if (isCI) exports.name = name
+})
+
+exports.isCI = !!(
+  env.CI || // Travis CI, CircleCI, Gitlab CI, Appveyor, CodeShip
+  env.CONTINUOUS_INTEGRATION || // Travis CI
+  env.BUILD_NUMBER || // Jenkins, TeamCity
+  exports.name ||
+  false
+)
---
+++ exercises/node_modules/realpath-native/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const fs = require('fs');
+const promisify = require('util.promisify');
+
+const promisiedFsRealpath = promisify(fs.realpath);
+
+function realpath(filepath) {
+  if (typeof fs.realpath.native === 'function') {
+    return promisify(fs.realpath.native)(filepath);
+  }
+  const fsBinding = process.binding('fs');
+
+  if (fsBinding.realpath) {
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(fsBinding.realpath(filepath, 'utf8'));
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+
+  return promisiedFsRealpath(filepath);
+}
+
+function realpathSync(filepath) {
+  if (typeof fs.realpathSync.native === 'function') {
+    return fs.realpathSync.native(filepath);
+  }
+
+  const fsBinding = process.binding('fs');
+
+  if (fsBinding.realpath) {
+    return process.binding('fs').realpath(filepath, 'utf8');
+  }
+
+  return fs.realpathSync(filepath);
+}
+
+module.exports = realpath;
+module.exports.sync = realpathSync;
