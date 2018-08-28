659cd3c0f7c9780247e4d394b4f5cb96d4b37fd6
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-08-27 19:16:35 -0700

    reverse

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+function reverse(str) {
+    return str.split('').reduce((rev, char) => char + rev, '')
+}
+function reverse(str) {
+    newArr = []
+    for (let i = str.length - 1; i >= 0; i--) {
+        newArr.push(str[i])
+    }
+    return (newArr.join(''))
+}
+function reverse(str) {
+    let reversed = '';
+
+    for (let char of str) {
+        reversed = char + reversed;
+    }
+    return reversed;
+}
 
 module.exports = reverse;
