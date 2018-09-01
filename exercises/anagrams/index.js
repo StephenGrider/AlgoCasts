030f6c15a9d694cb8f3494c0a6f0d13a429bc5a9
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-09-01 14:18:59 -0700

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,12 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+}
 
 module.exports = anagrams;
