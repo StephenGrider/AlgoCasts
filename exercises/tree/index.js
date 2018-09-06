688c704007e46ef1c5f55d1dd240168e44eeae6a
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-09-05 20:43:16 -0700

    tree

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,53 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.children = [];
+    }
 
-class Tree {}
+    add(data) {
+        const node = new Node(data);
+        this.children.push(node);
+    }
+
+    remove(data) {
+        this.children = this.children.filter(node => {
+            return node.data !== data;
+        })
+    }
+}
+
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    traverseBF(fn) {
+        const arr = [this.root];
+
+        while (arr.length) {
+            const node = arr.shift(); // returns first removed node
+
+            // arr.push(node.children) // cannot do this as it creates new arr inside arr, so we can use the following code:
+            // for (let child of node.children) {
+            //     arr.push(child);
+            // }
+            // or ES6 spread operator: arr.push(...node.children)
+            arr.push(...node.children);
+            fn(node) // for testing purpose
+        }
+    }
+    traverseDF(fn) {
+        const arr = [this.root];
+
+        while (arr.length) {
+            const node = arr.shift(); // returns first removed node
+            arr.unshift(...node.children);
+            fn(node) // for testing purpose
+        }
+    }
+}
 
 module.exports = { Tree, Node };
