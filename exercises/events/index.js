d5ca0f0cb9ea46337bdacbdc297a2b2fb8a3d034
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-09-09 09:41:08 -0700

    events

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,35 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
-  // Register an event handler
-  on(eventName, callback) {}
+    constructor() {
+        this.events = {};
+    }
+    // Register an event handler
+    on(eventName, callback) {
+        if (this.events[eventName]) {
+            this.events[eventName].push(callback);
+        } else {
+            this.events[eventName] = [callback];
+        }
+    }
 
-  // Trigger all callbacks associated
-  // with a given eventName
-  trigger(eventName) {}
+    // Trigger all callbacks associated
+    // with a given eventName
+    trigger(eventName) {
+        if (this.events[eventName]) {
+            for (let cb of this.events[eventName]) {
+                cb();
+            }
+        }
+    }
 
-  // Remove all event handlers associated
-  // with the given eventName
-  off(eventName) {}
+    // Remove all event handlers associated
+    // with the given eventName
+    off(eventName) {
+        // to remove entire array of cb
+        // this.events[eventName] = []; or
+        delete this.events[eventName];
+    }
 }
 
 module.exports = Events;
--- exercises/validate/test.js
+++ exercises/validate/test.js
@@ -2,22 +2,22 @@ const Node = require('./node');
 const validate = require('./index');
 
 test('Validate recognizes a valid BST', () => {
-  const n = new Node(10);
-  n.insert(5);
-  n.insert(15);
-  n.insert(0);
-  n.insert(20);
+    const n = new Node(10);
+    n.insert(5);
+    n.insert(15);
+    n.insert(0);
+    n.insert(20);
 
-  expect(validate(n)).toEqual(true);
+    expect(validate(n)).toEqual(true);
 });
 
 test('Validate recognizes an invalid BST', () => {
-  const n = new Node(10);
-  n.insert(5);
-  n.insert(15);
-  n.insert(0);
-  n.insert(20);
-  n.left.left.right = new Node(999);
-
-  expect(validate(n)).toEqual(false);
+    const n = new Node(10);
+    n.insert(5);
+    n.insert(15);
+    n.insert(0);
+    n.insert(20);
+    n.left.left.right = new Node(999);
+    // n.right.right.right = new Node(2)
+    expect(validate(n)).toEqual(false);
 });
