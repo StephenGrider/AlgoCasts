50924a2c60308a2fa3f5597542d80137a7976c86
ryanscottjohnson <teacherman1@gmail.com>
2018-09-10 14:42:45 -0700

    iterative solution to fibonacci series problem

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,7 +8,17 @@
 // Example:
 //   fib(4) === 3
 
-// Solution
-function fib(n) {}
+// Solution - Iterative
+function fib(n) {
+  const result = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    const a = result[i -1];
+    const b = result[i -2];
+
+    result.push(a + b);
+  }
+  return result[n];
+}
 
 module.exports = fib;
