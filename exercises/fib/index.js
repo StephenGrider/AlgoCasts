dd85ae02bd4b2ab59d79d879a436cb9dfeeeabb1
ryanscottjohnson <teacherman1@gmail.com>
2018-09-11 15:40:16 -0700

    comparing, refactoring, refining and testing fibonacci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,28 +9,28 @@
 //   fib(4) === 3
 
 // Solution - Iterative
-// RunTime Complexity = N (Linier)
-// function fib(n) {
-//   const result = [0, 1];
+RunTime Complexity = N (Linier)
+function fib(n) {
+  const result = [0, 1];
 
-//   for (let i = 2; i <= n; i++) {
-//     const a = result[i -1];
-//     const b = result[i -2];
+  for (let i = 2; i <= n; i++) {
+    const a = result[i -1];
+    const b = result[i -2];
 
-//     result.push(a + b);
-//   }
-//   return result[n];
-// }
+    result.push(a + b);
+  }
+  return result[n];
+}
 
 // Solution - Recursive
-// RunTime Complexity = 2^n (Exponential)
-// multiple identical function calls
-// function fib(n) {
-//   if (n < 2) {
-//     return n;
-//   }
-//   return fib(n - 1) + fib(n - 2);
-// }
+RunTime Complexity = 2^n (Exponential)
+multiple identical function calls
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
 
 // Solution - memoization
 function memoize(fn) {
