c9ac345581a9453fcd1a19f597eb1fda25734dd1
ryanscottjohnson <teacherman1@gmail.com>
2018-09-13 14:54:09 -0700

    linked list insertFirst method

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -13,6 +13,17 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
 }
+
+// // Mini Test for Repl.it
+// const nodeOne = new Node(5);
+// const list = new LinkedList();
+// list.head = nodeOne;
+// list.insertFirst(3);
+// list;
 
 module.exports = { Node, LinkedList };
