6fbb61702b7e238a6b693b19c78e1fea88e66045
ryanscottjohnson <teacherman1@gmail.com>
2018-09-20 20:22:02 -0700

    node constructor for trees

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,7 +10,12 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+}
 
 class Tree {}
 
