f423bbc2b91f670993bf5146298b4a6a35a84f73
ryanscottjohnson <teacherman1@gmail.com>
2018-09-21 11:15:55 -0700

    levelWidth breth first tree traversal

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+// width = breth first traversal
+function levelWidth(root) {
+  let arr = [root, 'stop'];
+  let counter = [];
+
+  while (arr.length > 1) {
+    const node = arr.shift();
+ 
+    if (node  === 'stop') {
+      counters.push(0);
+      arr.push('stop');
+    } else {
+      arr.push(...node.children);
+      counter[counter.length - 1]++;
+    }
+  }
+  
+  return counter;
+}
 
 module.exports = levelWidth;
