89d407c9351d4d68205110a9ebcd30df7bad9947
ryanscottjohnson <teacherman1@gmail.com>
2018-09-24 14:13:57 -0700

    binary search tree, insert method

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,29 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  // recursive method
+  insert(data) {
+    // data < node and node exists
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+      //data < node, create new node
+    } else if (data < this.data) {
+      this.left = new Node(data);
+      // data > node and node exists
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+      //data > node, create new node
+    } else if (data > this.data) {
+      this.right = new Node (data);
+    }
+  }
+}
 
 module.exports = Node;
