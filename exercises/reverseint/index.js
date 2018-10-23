46ffe0d8100104344bdb20124025a620dc3869dd
Kelly L <lock2846@gmail.com>
2018-10-23 17:51:39 -0600

    added three(ish) solutions with notes (two with Math.sign(), one without

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,35 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// num -> string -> array -> reverse -> string -> num
+// if n is less than 0, take num and muliple by -1
+// otherwise return reversed positive num
+
+function reverseInt(n) {
+  let reverse = parseInt(n.toString().split('').reverse().join(''))
+
+  if (n < 0) {
+    return reverse * -1
+  }
+  return reverse
+}
 
 module.exports = reverseInt;
+
+
+// num -> string -> array -> reverse -> string -> num
+// take reverse num and checks if num is pos or neg and multiplies it by 1 or -1
+
+// function reverseInt(n) {
+//   let reverse = parseInt(n.toString().split('').reverse().join(''))
+//
+//   return reverse * Math.sign(n)
+// }
+
+// OR
+
+// function reverseInt(n) {
+//   return (
+//     parseInt(n.toString().split('').reverse().join(''))
+//   ) * Math.sign(n)
+// }
