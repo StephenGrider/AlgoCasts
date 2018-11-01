9966fdccce49e137dd73f6f24e1db95115fe97ea
lawki <prakharpalpfdr@gmail.com>
2018-11-01 12:09:41 +0530

    test case when the linkedlist is empty

--- completed_exercises/linkedlist/test.js
+++ completed_exercises/linkedlist/test.js
@@ -53,6 +53,7 @@ describe('GetFirst', () => {
 describe('GetLast', () => {
   test('returns the last element', () => {
     const l = new List();
+    expect(l.getLast()).toEqual(null);
     l.insertFirst(2);
     expect(l.getLast()).toEqual({ data: 2, next: null });
     l.insertFirst(1);
