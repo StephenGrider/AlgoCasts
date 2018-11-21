96487f9e565f2dfd1f3af441fb9becbb173074a8
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-11-21 12:46:06 -0700

    Add palindrome solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,16 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// function palindrome(str) {
+//   return str === str.split('').reduce((reversed, char) => char + reversed, '');
+// }
+
+function palindrome(str) {
+  return str.split('').every((char, i) => char === str[str.length - i - 1])
+}
+
+function palindrome(str) {
+
+}
 
 module.exports = palindrome;
