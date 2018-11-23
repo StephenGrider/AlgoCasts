e0caf347da55d1776e49c561214a27980bf03a70
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-11-22 17:47:22 -0700

    Add solution for steps challenge

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,20 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let step = '';
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        step += '#';
+      } else {
+        step += ' '
+      }
+    }
+    
+    console.log(step);
+   
+  }
+}
 
 module.exports = steps;
