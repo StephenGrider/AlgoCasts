7414fda5f6c2603eeb18c26297e59cd4b3c200a5
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-11-24 15:18:01 -0700

    Add regular expression solution for vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,9 +7,14 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
+// function vowels(str) {
+//   const vowels = 'aeiou'
+//   return str.split('').filter(letter => vowels.includes(letter.toLowerCase())).length;
+// }
+
 function vowels(str) {
-  const vowels = 'aeiou'
-  return str.split('').filter(letter => vowels.includes(letter.toLowerCase())).length;
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
