8df1474cd776891e065c3ca6b72f0c2133b28dea
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-11-28 13:20:15 -0700

    More spiral practice

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -17,47 +17,45 @@
 
 function matrix(n) {
   const results = [];
-
   for (let i = 0; i < n; i++) {
     results.push([]);
   }
-  
+
   let counter = 1;
-  let startColumn = 0;
-  let endColumn = n - 1;
+
   let startRow = 0;
   let endRow = n - 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
 
-  while(startColumn <= endColumn && startRow <= endRow) {
-    //Top row
-    for (let i = startColumn; i<= endColumn; i++) {
+  while(startRow <= endRow && startColumn <= endColumn) {
+    //Top Row
+
+    for (let i = startColumn; i <= endColumn; i++) {
       results[startRow][i] = counter;
       counter++;
     }
-    startRow++;
+    startRow++
 
-    //Right column
-    for(let i = startRow; i <= endRow; i++) {
+    for (let i = startRow; i <= endRow; i++) {
       results[i][endColumn] = counter;
       counter++;
     }
     endColumn--;
 
-    //Bottom Row
-    for(let i = endColumn; i >= startColumn; i--) {
+    for (let i = endColumn; i >= startColumn; i--) {
       results[endRow][i] = counter;
       counter++;
     }
     endRow--;
-
-    //MiddleRow
     for (let i = endRow; i >= startRow; i--) {
       results[i][startColumn] = counter;
-      counter++;
+      counter++
     }
     startColumn++;
   }
-  return results
+    console.log(results);
+    return results;
 }
 
 module.exports = matrix;
