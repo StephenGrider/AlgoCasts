11066edd420b04e84b9675b179bba1a2497fe4ee
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-11-28 13:59:42 -0700

    Add fibbonaci solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -1,4 +1,4 @@
-// --- Directions
+ // --- Directions
 // Print out the n-th entry in the fibonacci series.
 // The fibonacci series is an ordering of numbers where
 // each number is the sum of the preceeding two.
@@ -8,6 +8,15 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  const result = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    const a = result[result.length -1];
+    const b = result[result.length - 2];
+    result[i] = a + b;
+  }
+  return result[n];
+}
 
 module.exports = fib;
