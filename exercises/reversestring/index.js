2d48077a60ebf99af0e34d05981559a639efa182
HeyNeek <nikoaguirre9315@gmail.com>
2018-11-28 23:35:59 -0600

    completed reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,25 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//apple
+//a + ""
+//p + "a"
+//p + "pa"
+//l + "lppa"
+
+
+function reverse(str) {
+  var newString = "";
+  
+  for (var i = 0; i < str.length; i++) {
+    newString = str[i] + newString;
+  }
+
+  return newString;
+}
 
 module.exports = reverse;
