1376f31b31a51e5b1496c5e9d5e25a2ae1d1ccdb
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-11-29 12:09:32 -0700

    More matrix practice

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,7 +15,13 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
+
+
+
+
+
 function matrix(n) {
+
   const results = [];
 
   for (let i = 0; i < n; i++) {
@@ -23,7 +29,6 @@ function matrix(n) {
   }
 
   let counter = 1;
-
   let startRow = 0;
   let endRow = n - 1;
   let startColumn = 0;
@@ -32,13 +37,15 @@ function matrix(n) {
   while(startRow <= endRow && startColumn <= endColumn) {
 
     //top row
-    for (let i = startColumn; i <= endColumn; i++) {
+
+    for(let i = startRow; i <= endColumn; i++) {
       results[startRow][i] = counter;
       counter++;
     }
-    startRow++;
-
+    startRow++
+    
     //right side
+
     for (let i = startRow; i <= endRow; i++) {
       results[i][endColumn] = counter;
       counter++
@@ -46,24 +53,83 @@ function matrix(n) {
     endColumn--;
 
     //bottom row
+
     for (let i = endColumn; i >= startColumn; i--) {
       results[endRow][i] = counter;
       counter++;
     }
     endRow--;
-
-    //middle
+    
+    // left
 
     for (let i = endRow; i >= startRow; i--) {
       results[i][startColumn] = counter;
-      counter++
+      counter++;
     }
     startColumn++;
-
-
-    console.log(results)
-    }
-    return results;
+  }
+  return results;
 }
+
+
+
+
+
+
+
+
+
+
+
+// function matrix(n) {
+//   const results = [];
+
+//   for (let i = 0; i < n; i++) {
+//     results.push([]);
+//   }
+
+//   let counter = 1;
+
+//   let startRow = 0;
+//   let endRow = n - 1;
+//   let startColumn = 0;
+//   let endColumn = n - 1;
+
+//   while(startRow <= endRow && startColumn <= endColumn) {
+
+//     //top row
+//     for (let i = startColumn; i <= endColumn; i++) {
+//       results[startRow][i] = counter;
+//       counter++;
+//     }
+//     startRow++;
+
+//     //right side
+//     for (let i = startRow; i <= endRow; i++) {
+//       results[i][endColumn] = counter;
+//       counter++
+//     }
+//     endColumn--;
+
+//     //bottom row
+//     for (let i = endColumn; i >= startColumn; i--) {
+//       results[endRow][i] = counter;
+//       counter++;
+//     }
+//     endRow--;
+
+//     //middle
+
+//     for (let i = endRow; i >= startRow; i--) {
+//       results[i][startColumn] = counter;
+//       counter++
+//     }
+//     startColumn++;
+
+
+//     console.log(results)
+//     }
+//     return results;
+// }
 
 module.exports = matrix;
