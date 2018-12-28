c1b411a93935c51ac38e3d10478e6cbd5f47aa58
Cody Taft <cody.taft@gmail.com>
2018-12-28 10:47:15 -0700

    Complete steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,5 +17,36 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+  steps(n, row, stair);
+}
+
 module.exports = steps;
+
+// function steps(n) {
+//   for (let i = 0; i < n; i++) {
+//     let stair = '';
+//     for (let j = 0; j < n; j++) {
+//       if (j <= i) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
