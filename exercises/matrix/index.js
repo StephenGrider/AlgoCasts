3ffc1637de2b2191024002067da2a26c4ea73311
Cody Taft <cody.taft@gmail.com>
2018-12-28 12:53:30 -0700

    Complete matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,47 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  results = [];
+  counter = 1;
+  startColumn = 0;
+  endColumn = n - 1;
+  startRow = 0;
+  endRow = n - 1;
+
+  for (i = 0; i < n; i++) {
+    results.push([]);
+  }
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    //Top row
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+
+    // Right Column
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+
+    // Bottom row
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+  return results;
+}
 
 module.exports = matrix;
