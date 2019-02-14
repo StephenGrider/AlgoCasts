2457bde9285e0b026ea075f21bd03e75b07766ed
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-13 17:50:45 -0700

    solved the palindrome problem

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,8 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
-
+function palindrome(str) {
+const reversed= str.split('').reduce((rev,char)=> char+rev ,'')
+return reversed === str
+}
 module.exports = palindrome;
