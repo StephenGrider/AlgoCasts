091965721142906eda2d816f1eb17a80d8ec1daf
vovka26 <317110@gmail.com>
2019-04-03 17:19:31 -0400

    solved first two problems

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,22 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((rev, char) => char + rev, '')
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//     return str.split('')
+//     .reverse()
+//     .join('')
+// }
+
+// function reverse(str) {
+//     let revStr = '';
+//     for(let char of str){
+//         revStr = char + revStr;
+//     }
+//     return revStr;
+// }
