b662d9d77d23edc4aa22fda707b1d031daf3c9c4
vovka26 <317110@gmail.com>
2019-04-05 15:46:04 -0400

    chunk problem

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,27 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const chunked = [];
+    let index = 0;
+    while(index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+    return chunked;
+}
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//     const chunked = [];
+//     for(let el of array){
+//         const last = chunked[chunked.length - 1]
+//         if(!last || last.length === size){
+//             chunked.push([el]);
+//         }else{
+//             last.push(el);
+//         }
+//     }
+//     return chunked;
+// }
