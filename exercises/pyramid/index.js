7968d3f91e8b6eef550dc847c4d0e6ef994354e4
vovka26 <317110@gmail.com>
2019-04-06 17:33:27 -0400

    pyramid problem

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,41 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+    
+    if (n === row) {
+        return;
+    }
+
+    if (level.length === 2 * n - 1) {
+        console.log(level);
+        return pyramid(n, row + 1)
+    }
+
+    const midPoint = Math.floor((2 * n - 1) / 2)
+
+    if (midPoint - row <= level.length && midPoint + row >= level.length) {
+        level += '#';
+    }else{
+        level += ' ';
+    }
+    pyramid(n, row, level)
+}
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//     const midPoint = Math.floor((2 * n - 1) /2)
+//     for(let row = 0; row < n; row++){
+//         let level = '';
+
+//         for(let col = 0; col < 2 * n - 1; col++){
+//             if (midPoint - row <= col && midPoint + row >= col) {
+//                 level += '#';
+//             }else{
+//                 level += ' ';
+//             }
+//         }
+//         console.log(level);
+//     }
+// }
