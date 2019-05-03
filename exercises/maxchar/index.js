754525c4d0b1d9f3d2f2d83e52f0443d803dc43d
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-03 11:13:06 -0700

    solution 1

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let outVal = '';
+    str = str.split('');
+    let chars = {};
+    str.forEach((val) => {
+        return !chars[val] ? chars[val] = 1 : chars[val]++;
+    })
+    let count = 0;
+    for (val in chars) {
+        if (chars[val] >= count) {
+            outVal = val;
+            count = chars[val];
+        }
+    }
+    console.log(chars, 'CHARS')
+    console.log(count, "COUNT")
+    console.log(outVal, 'OUTVAL')
+    return outVal;
+}
 
 module.exports = maxChar;
