71b0047b444b8adc3e53fccbc8246367dc9843dc
paristuzun <paristuzun@gmail.com>
2019-05-13 22:30:44 -0400

    created an object out of string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {}
+
+  for (let char of str ) {
+    if (!chars[char]) {
+      chars[char] = 1;
+    } else {
+      chars[char]++;
+      }
+  }
+
+  console.log(chars);
+
+}
 
 module.exports = maxChar;
