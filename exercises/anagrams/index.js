0dd4064c56983553c8387debab3e6196037c73cb
ckopecky <kopecky12112@gmail.com>
2019-05-20 15:21:17 -0700

    solution for anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,33 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const aCharMap = createCharMap(stringA);
+    const bCharMap = createCharMap(stringB);
+
+    function createCharMap(str){
+        let regex = /[^\w]/g;
+        const charMap = {};
+        for(let char of str.replace(regex, "").toLowerCase()) {
+            charMap[char] = charMap[char] + 1 || 1;
+        }
+
+        return charMap;
+    }
+
+    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+        return false;
+    }
+
+    for(let char in aCharMap) {
+        if (aCharMap[char] !== bCharMap[char]) {
+            return false;
+        }
+        return true;
+    }
+
+
+
+}
 
 module.exports = anagrams;
