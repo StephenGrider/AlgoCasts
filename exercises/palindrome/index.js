f3dbe1a9853fdbb82605ddf24a630603e5e4a59c
ckopecky <kopecky12112@gmail.com>
2019-05-20 09:00:06 -0700

    palindrome solved

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,27 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+/*
+Alternate solution: Not an ideal solution since it's twice as much work. 
 
+every array helper:
+
+every() does a boolean check on a condition. If there is any false checks, it will return false. If all checks are true, it will return true
+
+function palindrome(str){
+    return str.split('').every((char, index) => {
+        return char[index] === str[str.length - index - 1];
+    });
+    
+}
+
+
+
+*/
+
+function palindrome(str) {
+    let reversed = str.split('').reverse('').join('');
+    return str === reversed;
+}
+    
 module.exports = palindrome;
