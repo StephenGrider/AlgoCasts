ace88da2541d0447a6c76d62fb570af5cef2d910
ckopecky <kopecky12112@gmail.com>
2019-05-22 14:37:24 -0700

    implement palindrome react app

---
+++ exercises/palindrome/palindrome/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Palindrome from './Palindrome';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Palindrome Checker...</h1>
+      <Palindrome placeholder="Enter a string..."/>
+    </div>
+  );
+}
+
+export default App;
---
+++ exercises/palindrome/palindrome/src/Palindrome.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class Palindrome extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: '',
+            check: ''
+        }
+    }
+    
+
+    palindrome(str) {
+        let reversed = str.split('').reverse('').join('');
+        if ( reversed === str) {
+            this.setState({check: `${str} is a palindrome`});
+        } else {
+            this.setState({check: `${str} is NOT a palindrome`});
+        }
+    }
+    handleChange = (event) => {
+        this.setState({[event.target.name]: event.target.value })
+    }
+
+    handleKeyPress = (event) => {
+        if(event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById('submit-button').click();
+        }
+    }
+
+    render() {
+        return (
+                <div className="string-container" >
+                <div className="input-button-container">
+                    <input placeholder={this.props.placeholder} onChange={this.handleChange} onKeyUp={this.handleKeyPress} type="text" name="input" value={this.state.input}/>
+                    <button id="submit-button" onClick={() => this.palindrome(this.state.input)}>Submit</button>
+                </div>
+                <div className="results-container">
+                    <span>Result: </span>     
+                    <strong>{this.state.check || "Enter a phrase above!"} </strong>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Palindrome;
