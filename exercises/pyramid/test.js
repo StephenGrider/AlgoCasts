18b30b081356825c86af694f4f9328a369e531e5
ckopecky <kopecky12112@gmail.com>
2019-05-29 16:17:42 -0700

    corrected spelling on test.js

--- exercises/pyramid/test.js
+++ exercises/pyramid/test.js
@@ -12,14 +12,14 @@ test('pyramid is a function', () => {
   expect(typeof pyramid).toEqual('function');
 });
 
-test('prints a pryamid for n = 2', () => {
+test('prints a pyramid for n = 2', () => {
   pyramid(2);
   expect(console.log.mock.calls[0][0]).toEqual(' # ');
   expect(console.log.mock.calls[1][0]).toEqual('###');
   expect(console.log.mock.calls.length).toEqual(2);
 });
 
-test('prints a pryamid for n = 3', () => {
+test('prints a pyramid for n = 3', () => {
   pyramid(3);
   expect(console.log.mock.calls[0][0]).toEqual('  #  ');
   expect(console.log.mock.calls[1][0]).toEqual(' ### ');
