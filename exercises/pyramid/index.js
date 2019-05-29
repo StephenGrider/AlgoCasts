d30c1d6de328d873c4c878abdf8e7174c81592ac
ckopecky <kopecky12112@gmail.com>
2019-05-29 16:41:42 -0700

    pyramid solved

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,7 +13,59 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//iterative
+// const pyramid = (n) => {
+//     //very similar to steps solution
+//     let midpoint = Math.floor((2 * n - 1)/2);
+//     //loop through rows
+//     for (let row = 0; row < n; row++) {
+//         //for each row we create, create an empty string
+//         let level = "";
+//         //loop through columns
+//         //number of columns == 2 * n - 1;
+//         for (let column = 0; column < (2 * n - 1); column++) {
+//             // if the difference of the midpoint and the row AND the sum of the midpoint and row is === the column we are to add a #
+//             if(midpoint - row <= column && midpoint + row >= column) {
+//                 //current column is within these bounds;
+//                 level += "#";
+//             }
+//             else {
+//                 //otherwise add a space
+//                 level += ' ';
+//             }
+//         }
+//         console.log(level);
+//     }
+// }
+//initiate a func where row = 0 and you have an empty string
+const pyramid = (n, row = 0, level="") => {
+    //assign length to a var
+    let length = 2 * n - 1;
+    //if row is equal to n you are done with recursion.
+    if(row === n) {
+        return;
+    } 
+    //if length is equal to length of string, start a new string with a new function call with 1 added to row
+    if(level.length === length) {
+        console.log(level);
+        return pyramid(n, row + 1);
+    }
 
-function pyramid(n) {}
+    //calc midpoint
+    let midpoint = Math.floor(length/2); //gives index
+    //instantiate var for the char that will be added to string
+    let add;
+    //
+    if (midpoint - row <= level.length && midpoint  + row >= level.length) {
+        add = "#";
+    } else {
+        add = " ";
+    }
+    //call pyramid with string + added char.
+    pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
+
+
+pyramid(6)
