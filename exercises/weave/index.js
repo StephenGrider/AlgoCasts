6f7b201b32b719b3c9e4603be1feebfbcc863a87
ckopecky <kopecky12112@gmail.com>
2019-05-31 08:10:54 -0700

    weave done

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -24,6 +24,19 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+function weave(sourceOne, sourceTwo) {
+    const q = new Queue;
+    
+
+    while(sourceOne.peek() || sourceTwo.peek()) {
+        if(sourceOne.peek()) {
+            q.add(sourceOne.remove());
+        }
+        if(sourceTwo.peek()) {
+            q.add(sourceTwo.remove());
+        }
+    }
+    return q;
+}
 
 module.exports = weave;
