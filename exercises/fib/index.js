237238778941bfb311d46adf8af6580df32325ad
ckopecky <kopecky12112@gmail.com>
2019-05-30 17:14:35 -0700

    figured out memoized version using recursion

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -7,7 +7,38 @@
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
+const cache = {'0': 0, '1': 1};
+const fib = (n) => {
+    if(n < 2 ){
+        return n;
+    }
+    else {
+        if(!cache[n] && n >= 2){
+            let nth = fib(n - 1) + fib(n - 2);
+            cache[n] = nth;
+            console.log(cache);
+            return cache[n];
+        }
+        else {
+            return cache[n];
+        }
+    }
+        
 
-function fib(n) {}
+}
 
 module.exports = fib;
+
+//iterative
+
+//create an array
+// const fibArr = [0, 1];
+// //O(n) runtime
+// for (let i = 2; i <= n; i++) {
+//     const prev = fibArr[i - 1];
+//     const prevprev = fibArr[i - 2];
+
+//     fibArr.push(prev + prevprev);
+//}
+
+console.log(fib(55));
