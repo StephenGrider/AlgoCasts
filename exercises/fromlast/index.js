537a156e431403590d5a9ffb861d8656912da4d7
ckopecky <kopecky12112@gmail.com>
2019-06-05 16:25:44 -0700

    fromlast

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -1,3 +1,4 @@
+const LL = require('./linkedlist');
 // --- Directions
 // Given a linked list, return the element n spaces
 // from the last node in the list.  Do not call the 'size'
@@ -11,6 +12,33 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+
+//step 1: advance fast by n spaces
+//step 2: leave slow alone
+//step 3: advance fast and slow by 1
+//step 4: is fast at last element? 
+//if so, slow == nth from end.
+//if not, advance again. 
+
+function fromLast(list, n) {
+    let fast = list.head;
+    let slow = list.head;
+
+    for (let i = 0; i < n; i++) {
+        fast = fast.next;
+    }
+
+    while(fast.next) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+    return slow;
+
+
+    
+
+    
+
+}
 
 module.exports = fromLast;
