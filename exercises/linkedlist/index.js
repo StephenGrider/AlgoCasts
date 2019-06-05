963e87535a51baf652f7b78c4bf16beb9e366abb
ckopecky <kopecky12112@gmail.com>
2019-06-05 11:23:03 -0700

    midpoint and linkedlist

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -1,44 +1,197 @@
 class Node {
-    constructor(data, next = null) {
-        this.data = data;
-        this.next = next;
-    }
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
 class LinkedList {
-    //linked list class only knows about the first node
-    constructor() {
-        this.head = null;
+  //linked list class only knows about the first node
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    if (!this.head) {
+      this.head = new Node(data, this.head);
+    } else {
+      this.next = this.head;
+      this.head = new Node(data, this.next);
+    }
+  }
+
+  size() {
+    let node = this.head;
+    if (!this.head) {
+      return 0;
+    } else {
+      let counter = 0;
+      while (node) {
+        counter++;
+        node = node.next;
+      }
+      return counter;
+    }
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    let node = this.head;
+    if (!node) {
+      return null;
+    }
+    while (node) {
+      if (node.next !== null) {
+        node = node.next;
+      } else {
+        return node;
+      }
+    }
+    return node;
+  }
+
+  clear() {
+    this.head = null;
+    return this.head;
+  }
+
+  removeFirst() {
+    //no head? return null
+    if (!this.head) {
+      return;
+    } else {
+      //head exists
+      //does this.next === null ?
+      if (!this.head.next) {
+        //means this.head is first
+        this.head.next = null;
+        this.head = this.head.next;
+        return this.head;
+      } else {
+        this.head = this.head.next;
+        return this.head;
+      }
+    }
+  }
+
+  removeLast() {
+    if (!this.head) {
+      //this.head does not exist;
+      return;
+    }
+    if (this.size() === 1) {
+      this.head = null;
+      return;
+    } else {
+      //use helper to get Last node
+      let last = this.getLast();
+      //assign node to this.head
+      let node = this.head;
+      //if no last return null
+      //while node exists...
+      while (node) {
+        if (node.next === last) {
+          node.next = null;
+          return node;
+        }
+
+        if (node.next !== last) {
+          node = node.next;
+        }
+      }
+    }
+  }
+
+  insertLast(data) {
+    let last = this.getLast();
+    if (!last) {
+      this.head = new Node(data);
+    } else {
+      last.next = new Node(data);
+    }
+  }
+
+  getAt(int) {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (int === counter) {
+        return node;
+      } else {
+        counter++;
+        node = node.next;
+      }
+    }
+    return null;
+  }
+
+  removeAt(int) {
+    if (this.size() < int || !this.head) {
+      return;
+    }
+    const prev = this.getAt(int - 1);
+    const next = this.getAt(int + 1);
+
+    if (!prev) {
+      let node = this.head;
+      node = null;
+      this.head = next;
+      return this.head;
     }
 
-    insertFirst(data) {
-        if(!this.head) {
-            console.log("no head")
-            this.head = new Node(data, this.head);
-        }
-        else {
-            this.next = this.head;
-            this.head = new Node(data, this.next);
-        }
+    if (!prev.next) {
+      return this.removeLast();
+    }
+    prev.next = next;
+    return prev;
+  }
+
+  insertAt(data, int) {
+    //takes care of empty list
+    if (!this.head) {
+      return this.insertFirst(data);
+    }
+    //takes care of int decalred outside of size
+    if (int > this.size()) {
+      return this.insertLast(data);
     }
 
-    size() {
-        if(!this.head) {
-            return 0;
-        } else {
-            
-        }
+    if (int === 0) {
+      this.head = new Node(data, this.head);
+      return this.head;
     }
+
+    //gets previous node
+    let prev = this.getAt(int - 1);
+    //gets next node
+    //assigns new Node with data and a next value
+    prev.next = new Node(data, prev.next);
+  }
+
+  forEach(func) {
+    let node = this.head;
+    if (!this.head) {
+      return 0;
+    } else {
+      while (node) {
+        func(node);
+        node = node.next;
+      }
+    }
+  }
+
+  //optional...
+  *[Symbol.iterator]() {
+    let node = this.head;
+    while (node) {
+      yield node;
+      node = node.next;
+    }
+  }
 }
-
-const list = new LinkedList();
-list.insertFirst(10);
-list.insertFirst(10);
-list.insertFirst(10);
-list.insertFirst(10);
-console.log(list);
-console.log(list.size());
-
 
 module.exports = { Node, LinkedList };
 
@@ -58,4 +211,3 @@ module.exports = { Node, LinkedList };
 
 // console.log(NodeOne);
 // console.log(nodeTwo);
-
--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -12,6 +12,31 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+function midpoint(list) {
+        //otherwise, assign fast and slow to the first element
+        let fast = list.head;
+        let slow = list.head;
+        while(fast.next && fast.next.next) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+}
 
 module.exports = midpoint;
+
+
+/*
+
+Create two temp vars. Point them both to the first node in the linked list. 
+
+Start to iterate through the linked list. For every step of the while loop, advance one var forward  by 1 element, advance the other by two elements. 
+
+As soon as we advance fast to the next node, we will check to see if the next two nodes exist...if they do exist, we still continue to iterate through the list. When fast is no longer existing, slow will be at the midpoint. 
+
+
+
+
+
+
+*/
