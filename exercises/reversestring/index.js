bc04bb2af1d472bdb4c7c565840ca57546c7f47c
bobbyvidal <bobbyvidal@gmail.com>
2019-07-01 14:18:42 -0500

    reversed string done

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+
+    return str.split('').reduce((reversed, character) => {
+        return character + reversed
+    }, '')
+
+}
 
 module.exports = reverse;
+
+// first solution
+// return str.split("").reverse().join("");
+//////////////////////////////////////////////////////////
+
+// second solution
+// let reversed = ''
+
+// for (let character of str) {
+//     reversed = character + reversed
+// }
+
+// return reversed
+////////////////////////////////////////////////////////
+
