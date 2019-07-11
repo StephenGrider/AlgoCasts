13b127add68f84529a150afff4b13be723132309
Joe Szafarowicz <joeszaf+github@gmail.com>
2019-07-11 14:56:50 -0400

    Max char - joe attempt

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+
+// Joe attempt
+function maxChar(str) {
+  let chars = {}
+
+  str.split("").forEach(char => {
+    if (!chars[char]) {
+      chars[char] = 1
+    } else {
+      chars[char] = chars[char] + 1
+    }
+  })
+
+  let highestCount = -1
+  let mostFrequentChar = ""
+
+  Object.keys(chars).map((key) => {
+    if (chars[key] > highestCount){
+      highestCount = chars[key]
+      mostFrequentChar = key
+    }
+  })
+
+  return mostFrequentChar
+}
 
 module.exports = maxChar;
