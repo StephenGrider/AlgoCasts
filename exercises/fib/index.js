40c01907e678c27ea728aea9d7b289d8bc0a763d
bobbyvidal <bobbyvidal@gmail.com>
2019-08-08 16:39:34 -0500

    finished fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,16 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+    const result = [0,1];
+
+    for(let i = 2; i<=n; i++){
+        const a = result[i-1]
+        const b = result[i-2]
+        result.push(a+b)
+
+    }
+    return result[n]
+}
 
 module.exports = fib;
