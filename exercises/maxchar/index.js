1defe51fbf14c1845708490ff07b95ad2f35db12
niesssiobhan <niesssiobhan@live.com>
2019-08-31 13:36:32 -0700

    done with problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,8 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-const maxChar = (str) => {
-  
-}
+const maxChar = str => {
+  let charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+};
 
 module.exports = maxChar;
