8cda72b7adf3d8cefdb373e05da2baf458601cbb
niesssiobhan <niesssiobhan@live.com>
2019-09-04 11:16:57 -0700

    starting point

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,8 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+const chunk = (array, size) => {
+   
+};
 
 module.exports = chunk;
