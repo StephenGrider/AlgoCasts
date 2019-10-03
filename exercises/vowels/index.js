82ae23d8085755ed8950fbac830e225dd030e29c
github username <github email address>
2019-10-03 15:57:51 -0700

    vowels complete

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,23 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    let lowerCase = str[i].toLowerCase();
+    if (
+      lowerCase === "a" ||
+      lowerCase === "e" ||
+      lowerCase === "i" ||
+      lowerCase === "o" ||
+      lowerCase === "u"
+    ) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+//O(n) runtime O(1) space complexity
 
 module.exports = vowels;
