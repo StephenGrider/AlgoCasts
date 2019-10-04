30592bc2e60bdf915541ca60c41a095b7dac18e2
github username <github email address>
2019-10-04 08:55:58 -0700

    fizzbuzz, reverse string, palindrome

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,20 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+console.log(fizzBuzz(5));
 
 module.exports = fizzBuzz;
