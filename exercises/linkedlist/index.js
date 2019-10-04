9f130ab49c153b8db7623ed41108dcc283946673
bobbyvidal <bobbyvidal@gmail.com>
2019-10-04 13:57:20 -0400

    finished insertAt

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -129,6 +129,22 @@ class LinkedList {
         previous.next = previous.next.next;
       }
 
+      insertAt(data, index) {
+        if (!this.head) {
+          this.head = new Node(data);
+          return;
+        }
+    
+        if (index === 0) {
+          this.head = new Node(data, this.head);
+          return;
+        }
+    
+        const previous = this.getAt(index - 1) || this.getLast();
+        const node = new Node(data, previous.next);
+        previous.next = node;
+      }
+
     
 }
 
