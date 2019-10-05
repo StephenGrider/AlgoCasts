806010e0f5ec29173db8ef1f8f22e0356f28e2fa
github username <github email address>
2019-10-05 15:07:10 -0700

    array chunk completed

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,21 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let output = [];
+  let index = 0;
+
+  while (index < array.length) {
+    let chunk = array.slice(index, index + size);
+    output.push(chunk);
+    index += size;
+  }
+
+  return output;
+}
+
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5));
+
+//O(n) runtime, O(1) space complexity?
 
 module.exports = chunk;
