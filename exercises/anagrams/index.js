527226539e568b635eefacefaab2a311a364ffd8
github username <github email address>
2019-10-07 09:03:48 -0700

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,58 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+//use regex to remove any punctuation
+//check that lengths of inputs are equal
+//lowercase each input
+//create object tally for input A
+//create object tally for input B
+//check if obj[key] is equal in both objects
+
+function anagrams(stringA, stringB) {
+  let inputA = stringA.replace(/[^\w]/g, "").toLowerCase();
+  let inputB = stringB
+    .replace(/[\s.,/\#!S%\^&\*;:{}=\-_`~()]/g, "")
+    .toLowerCase();
+  if (inputA.length !== inputB.length) {
+    return false;
+  }
+  console.log(inputA);
+  console.log(inputB);
+  let objectA = {};
+  let objectB = {};
+
+  for (let i = 0; i < inputA.length; i++) {
+    if (!objectA[inputA[i]]) {
+      objectA[inputA[i]] = 1;
+    } else {
+      objectA[inputA[i]] += 1;
+    }
+  }
+
+  //O(a) time complexity
+
+  for (let i = 0; i < inputB.length; i++) {
+    if (!objectB[inputB[i]]) {
+      objectB[inputB[i]] = 1;
+    } else {
+      objectB[inputB[i]] += 1;
+    }
+  }
+
+  //O(b) time complexity
+
+  for (let key in objectA) {
+    if (objectA[key] !== objectB[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//O(a) time complexity
+
+console.log(anagrams("rail Safety", "fairy Tales"));
+
+//O(b + 2a) which is O(b + a) time complexity, O(ab) space complexity?
 
 module.exports = anagrams;
