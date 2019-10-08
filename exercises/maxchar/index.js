6cc2e98eb5dbd5e0f318eb0d3bf47b815f3e20f7
Joe <yogurmystery@gmail.com>
2019-10-08 15:34:47 -0500

    most used char in a string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,15 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charsObj = {};
+  const charsArr = str.split('');
+  charsArr.forEach((element) => {
+    charsObj[element] ? charsObj[element] += 1 : charsObj[element] = 1;
+  });
+  const entries = Object.entries(charsObj);
+  const sorted = entries.sort((a, b) => a[1] + b[1]);
+  return sorted[0][0];
+}
 
 module.exports = maxChar;
