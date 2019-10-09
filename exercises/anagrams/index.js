1e6dba0fa3d2cf97774aa057d896f88bb94681f1
Abdelrahman Hafez <a.hafez852@gmail.com>
2019-10-09 14:55:44 +0200

    Solve anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams (stringA, stringB) {}
+function anagrams (stringA, stringB) {
+  const stringALetters = getLettersArray(stringA);
+  const stringBLetters = getLettersArray(stringB);
+
+  const stringAOccurenceCountObject = getOccurenceCountObject(stringALetters);
+  const stringBOccurenceCountObject = getOccurenceCountObject(stringBLetters);
+
+  const allLettersFromAAreUsedTheSameAsB = stringIsPartialOfAnotherString(stringAOccurenceCountObject, stringBOccurenceCountObject);
+  const allLettersFromABreUsedTheSameAsA = stringIsPartialOfAnotherString(stringBOccurenceCountObject, stringAOccurenceCountObject);
+
+  return allLettersFromAAreUsedTheSameAsB && allLettersFromABreUsedTheSameAsA;
+}
+
+function stringIsPartialOfAnotherString (stringAOccurenceObject, stringBOccurenceObject) {
+  return Object.entries(stringAOccurenceObject).every(([letter, count]) => stringBOccurenceObject[letter] === count);
+}
+
+
+function getOccurenceCountObject (lettersArray) {
+  return lettersArray.reduce((occurenceCountObject, letter) => {
+    if (occurenceCountObject[letter]) occurenceCountObject[letter]++;
+    else occurenceCountObject[letter] = 1;
+
+    return occurenceCountObject;
+  }, {});
+}
+
+function getLettersArray (string) {
+  return string.split('').filter(char => /[a-zA-Z]/.test(char));
+}
 
 module.exports = anagrams;
