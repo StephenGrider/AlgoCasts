6e56b52989a92c499dd803315bf8083b23ce91f1
Abdelrahman Hafez <a.hafez852@gmail.com>
2019-10-10 12:59:24 +0200

    Add palindrome solutions

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,28 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome (str) {}
+// function palindrome (str) {
+//   const reversed = str.split('').reverse().join('');
+//   return str === reversed;
+// }
+
+// function palindrome (str) {
+//   for (let i = 0; i < str.length / 2; i++) {
+//     if (str[i] !== str[str.length - 1 - i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+
+// function palindrome (str) {
+//   return str.split('').every((character, i) => character === str[str.length - 1 - i]);
+// }
+
+function palindrome (str) {
+  const theFirstHalfOfTheString = str.slice(0, Math.floor(str.length / 2));
+  return theFirstHalfOfTheString.split('').every((character, i) => character === str[str.length - 1 - i]);
+}
 
 module.exports = palindrome;
