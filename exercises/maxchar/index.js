f1bcbbd1fccd7231f1567fff4ea7a3ae88d0bf90
Abdelrahman Hafez <a.hafez852@gmail.com>
2019-10-10 14:33:51 +0200

    Add solution to maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar (str) {}
+function maxChar (str) {
+  const charactersUsageCount = str.split('').reduce((charactersUsageCount, character) => {
+    if (!charactersUsageCount[character]) charactersUsageCount[character] = 1;
+    else charactersUsageCount[character]++;
+    return charactersUsageCount;
+  }, {});
+
+  const [[mostUsedCharacter]] = Object.entries(charactersUsageCount)
+    .sort(([charA, charAUsage], [charB, charBUsage]) => {
+      if (charAUsage > charBUsage) return -1;
+      if (charAUsage < charBUsage) return 1;
+      return 0;
+    });
+
+  return mostUsedCharacter;
+}
 
 module.exports = maxChar;
