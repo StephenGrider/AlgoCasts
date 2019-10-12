342312c5326975e2c04033e5c806636c2e4c68f0
Hafez <a.hafez852@gmail.com>
2019-10-12 17:55:46 +0200

    Add solution to chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,16 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk (array, size) {}
+function chunk (array, size) {
+  const chunks = [];
+
+  while (array.length > size) {
+    chunks.push(array.splice(0, size));
+  }
+
+  chunks.push(array);
+
+  return chunks;
+}
 
 module.exports = chunk;
