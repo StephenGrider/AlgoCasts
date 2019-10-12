1198af251fa74c509622a5c1582e29efd87af328
Hafez <a.hafez852@gmail.com>
2019-10-12 17:50:49 +0200

    Add solution to capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,8 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize (str) {}
+function capitalize (str) {
+  return str.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
+}
 
 module.exports = capitalize;
