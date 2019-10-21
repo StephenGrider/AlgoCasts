44b6af51b89c4b4190678ee8cff74a23a932b21f
github username <github email address>
2019-10-21 16:43:50 -0700

    stacks

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,24 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  push(element) {
+    this.data.push(element);
+    return this.data;
+  }
+
+  pop(element) {
+    return this.data.pop(element);
+  }
+
+  peek() {
+    let element = this.data[this.data.length - 1];
+    return element;
+  }
+}
 
 module.exports = Stack;
