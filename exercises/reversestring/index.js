84a002a654b4e67972c39ea27df4fe30b6f9f8af
github username <github email address>
2019-10-22 23:10:39 -0500

    reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,50 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// turn the string into an array
+// reverse the array "array.prototype.reverse()"
+// restringify the array
+
+// function reverse(string) {
+//   const array = string.split("");
+//   array.reverse();
+//   return array.join("");
+// }
+
+// // shorter solution
+// function reverse(string) {
+//   return string
+//     .split("")
+//     .reverse()
+//     .join("");
+// }
+
+// alternative solution without the .reverse() function, uses a for loop
+
+// create an empty array string called 'reversed'
+// for each character in the provided string
+// take the character and add it to the start of the 'reversed'
+// return the variable 'reversed'
+
+function reverse(string) {
+
+
+}
+// function reverse(string) {
+
+// }
 
 module.exports = reverse;
+
+
+
+// REACTO
+
+// R - *Repeat* the question (make sure you understand the question)
+// E - Give *examples* (given these parameters what output do you expect)
+// A - Describe your *approaches* (help the interviewer understand the path and you can get a little feedback from interviewer's response)
+// C - *Code*... psudocode from higher level (Breath first coding from CTCI Book),
+// T - *Test* your code, Run some of the examples through the code, use another color to show the changing variables, show that you can find your own problems
+// O - *Optimization* and run time, see if you can refactor **Big O**
+
+//Extra tips - leave space to add and remove code because it isnt like a text editor, star things that you are not sure about
