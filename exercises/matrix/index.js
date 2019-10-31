4c246593232f579270e449a9dbda84725805f6c0
Hafez <a.hafez852@gmail.com>
2019-10-31 08:54:40 +0200

    Add solution to spiral matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,51 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix (n) {}
+function matrix (n) {
+  const result = Array.from({ length: n }).map(value => Array.from({ length: n }));
+
+  let rowsBeginning = 0;
+  let rowsEnd = n - 1;
+  let columnsBeginning = 0;
+  let columnsEnd = n - 1;
+
+  let currentNumber = 1;
+
+  while (rowsBeginning <= rowsEnd && columnsBeginning <= columnsEnd) {
+    // move right
+    for (let horizontalIndex = columnsBeginning; horizontalIndex <= columnsEnd; horizontalIndex++) {
+      result[rowsBeginning][horizontalIndex] = currentNumber;
+      currentNumber++;
+    }
+
+
+    // move down
+    for (let verticalIndex = rowsBeginning + 1; verticalIndex <= rowsEnd; verticalIndex++) {
+      result[verticalIndex][rowsEnd] = currentNumber;
+      currentNumber++;
+    }
+
+
+    // move left
+    for (let horizontalIndex = columnsEnd - 1; horizontalIndex >= columnsBeginning; horizontalIndex--) {
+      result[rowsEnd][horizontalIndex] = currentNumber;
+      currentNumber++;
+    }
+
+
+    // move up
+    for (let verticalIndex = rowsEnd - 1; verticalIndex >= rowsBeginning + 1; verticalIndex--) {
+      result[verticalIndex][columnsBeginning] = currentNumber;
+      currentNumber++;
+    }
+
+    columnsEnd--;
+    rowsEnd--;
+    columnsBeginning++;
+    rowsBeginning++;
+  }
+
+  return result;
+}
 
 module.exports = matrix;
