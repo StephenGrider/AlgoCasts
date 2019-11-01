3eaad4dbce37e8cbc48ce036642f3d802eabba12
Hafez <a.hafez852@gmail.com>
2019-11-01 07:57:37 +0200

    Add memoization to Fibonacci's recursive solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,9 +8,38 @@
 // Example:
 //   fib(4) === 3
 
+// function memoize (fn) {
+//   const cache = {};
+
+//   return function () {
+//     const stringifiedArguments = JSON.stringify(arguments);
+
+//     if (cache[stringifiedArguments]) return cache[stringifiedArguments];
+
+//     const value = fn.apply(null, arguments);
+//     cache[stringifiedArguments] = value;
+//     return value;
+//   };
+// }
+
+// const fib = memoize(slowFib);
+
+// function slowFib (n) {
+//   if (n < 2) return n;
+
+//   return fib(n - 1) + fib(n - 2);
+// }
+
 function fib (n) {
-  if (n < 2) return n;
-  return fib(n - 1) + fib(n - 2);
+  const result = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    const a = result[i - 1];
+    const b = result[i - 2];
+    result.push(a + b);
+  }
+
+  return result[n];
 }
 
 module.exports = fib;
