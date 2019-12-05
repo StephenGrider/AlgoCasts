0c89eb1e9e375a473a15af84698f2ec39b4e9b89
Maribel Duran <maribelduranucm@gmail.com>
2019-12-04 18:46:56 -0800

    2019 solution to problem

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -9,8 +9,25 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-let reversed = n.toString().split('').reverse().join('');
-return parseInt(reversed) * Math.sign(n);
+  //Solution 1: Got clues to use toString, parseInt, and Math.sign()
+  //Didn't know that parsInt would return a positive value for a string like "15-" so I
+  //removed the sign from the string initially.
+  const sign = Math.sign(n);
+  const reversed = (n * sign)
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+  return parseInt(reversed * sign);
+
+  //Solution 2: If I knew parseInt returns positive for "15-"
+  /*const reversed = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+  return parseInt(reversed) * Math.sign(n);
+  */
 }
 
 module.exports = reverseInt;
