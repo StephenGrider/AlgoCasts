8d19bdae05880c5741fb8e7e760d41c31ef3c1e8
Maribel Duran <maribelduranucm@gmail.com>
2019-12-04 16:52:35 -0800

    Solutions in 2019

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,29 +7,34 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    let reversed = '';
-//Solution 1: make a copy of the str and transform it into an array. pop each value fomr the copied array 
-//onto the new string
-/*
-    let strCopy = [...str.split("")]; 
-    while (strCopy.length > 0){
-        reversedStr += strCopy.pop();
-    }
-    */
+  //2019 solutions
+  /* Solution 1: Use the forEach helper method to loop through each character and create the
+                 a reversed copy of the input string
+  let reversed = '';
+  
+  str.split('').forEach(char => {
+    reversed = char + reversed;
+  })
 
-  //Solution 2: iterate through the array concatenate each value to the new string using forEach.
-   /* str.split('').forEach(function(val){
-        reversed = val + reversed; 
-    })
-    return reversed;
-*/
-    //solution 3: use reduce function 
-    return str.split("").reduce((reversed, char) => {
-        return char + reversed;
-    }, '');
+  return reversed; 
+  */
 
-    /* Solution 4: use the reduce array method to reverse the string.
-    return str.split('').reverse().join('');
+  /* Solution 2: Use the reduce method
+  let reversed = str.split("").reduce((reversed, currChar) => {
+    return currChar + reversed;
+  }, "");
+  
+  return reversed;
+  */
+
+  // Solution 3: simplify reduce even more
+  return str.split("").reduce((reversed, char) => char + reversed, "");
+
+  //Solution 4: Use the reverse method of an array
+  /*return str
+    .split("")
+    .reverse()
+    .join("");
     */
 }
 
