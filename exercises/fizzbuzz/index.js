cffbe5ea59af399020633cb60071c638a900e946
Maribel Duran <maribelduranucm@gmail.com>
2019-12-06 14:13:09 -0800

    2019 solution

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -13,17 +13,17 @@
 //   buzz
 
 function fizzBuzz(n) {
-	for (let i = 1; i<=n; i++){
-		if (i % 3 === 0 && i % 5 === 0) { 
-  		console.log("fizzbuzz");
-    }else if (i % 3 === 0){
-			console.log("fizz");
-		}else if (i % 5 === 0){
-			console.log("buzz");
-		}else{
-			console.log(i)
-		}
-  }      
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
 }
 
 module.exports = fizzBuzz;
