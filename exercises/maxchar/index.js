651dae17f9287a61e8d24bd5920d3448df5180da
Maribel Duran <maribelduranucm@gmail.com>
2019-12-05 22:23:14 -0800

    Update comments

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,14 +6,14 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-  // Create a hash to store the amount of times that each character is found in a string.
+  // Create a hash
   const charMap = str.split("").reduce((charMap, c) => {
     //charMap[c] = charMap[c] ? charMap[c] + 1 : 1;
     charMap[c] = charMap[c] + 1 || 1;
     return charMap;
   }, {});
 
-  //find the max value in the has properties and return its key value
+  //find the max value
   let max = 0;
   let maxChar = "";
   for (let char in charMap) {
