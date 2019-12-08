9c48761f294056b5ade632d88fdfd81422cd8b39
Maribel Duran <maribelduranucm@gmail.com>
2019-12-07 17:01:20 -0800

    2019 solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,33 +8,13 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
- let capitalized = [];
-  //split each word of the string into an array
-  //loop through the array and for each word, capitalize the first letter
-  str.split(' ').forEach(word => {
-      let newWord = word[0].toUpperCase() + word.slice(1);
-      capitalized = [...capitalized, newWord];
+  //Solution 1: Use slice method and toUpperCase function
+  const capitalized = str.split(" ").map(word => {
+    const firstLetter = word[0].toUpperCase();
+    const rest = word.slice(1);
+    return firstLetter + rest;
   });
-
-  return capitalized.join(' ');
-  /* Solution 2: using the reduce method
-  let capitalized = str.split(' ').reduce(function(cap, word){
-    cap.push(word[0].toUpperCase() + word.slice(1));
-    return cap;
-  },[]);
-  */
- /*Solution 3
-  let capitalized = '';
-  str.split('').forEach(function(char, index){
-    if (index === 0 || str[index-1] === ' '){
-      capitalized += char.toUpperCase();
-    }
-    else{
-      capitalized += char;
-    }
-  })
-  return capitalized;
-  */
+  return capitalized.join(" ");
 }
 
 module.exports = capitalize;
