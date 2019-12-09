1232a17b562587f00c78d98496077763143705be
maribelduran <maribelduranucm@gmail.com>
2019-12-09 13:49:40 -0800

    2019 solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,42 +14,46 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n, row=0, level='') {
-  if(row === n){
-    return; 
-  }
-  //if we are at the end of the column then we console log
-  //and go to the next row by calling pyramid again
-  let numOfCols = 2 * n - 1;
-  if (level.length === numOfCols){
-    console.log(level);
-    return pyramid(n, row+1);
-  }
-  let middlePoint = Math.floor(numOfCols/2);
-  let currentCol = level.length;
-  if ((currentCol >= (middlePoint-row)) && (currentCol <= (middlePoint+row))){
-    level+='#';
-  }
-  else {
-    level+=' ';
-  }
-  pyramid(n, row, level);  
-} 
-  /* Solution 1: Iterative solution
-  let numOfCols = 2 * n - 1;
-  let middlePoint = Math.floor((2 * n - 1)/2);
+function pyramid(n, row = 0, level = "") {
+  //Solution 1: Iterative
+  /*const midpoint = Math.floor((2 * n - 1) / 2);
 
-  for (let row=0; row<n; row++){
-    let level  = '';
-    for (let col=0; col<numOfCols; col++){
-      if ((col >= (middlePoint-row)) && (col <= (middlePoint+row))){
-        level+='#';
-    }
-      else{
-        level+=' ';
+  for (let row = 0; row < n; row++) {
+    let level = "";
+    for (let column = 0; column < 2 * n - 1; column++) {
+      //find the range where we should add a pound sign
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += "#";
+      } else {
+        level += " ";
       }
     }
     console.log(level);
-  } */
+  }
+  */
+
+  //Solution 2: use recursion
+  //base case: if we have passed the last row then we simply return
+  if (n === row) {
+    return;
+  }
+
+  const columns = 2 * n - 1;
+  //if we are the end of a column, then we print the current level and then go to the next row
+  if (columns === level.length) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const currentCol = level.length;
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  //check if the currentCol falls within the range we want to add #'s to
+  if (midpoint - row <= currentCol && midpoint + row >= currentCol) {
+    level += "#";
+  } else {
+    level += " ";
+  }
+  return pyramid(n, row, level);
+}
 
 module.exports = pyramid;
