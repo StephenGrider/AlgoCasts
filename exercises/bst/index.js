1c20d479c75d1825507b96c4fec0fe0d6b8c2bfd
Hafez <a.hafez852@gmail.com>
2019-12-18 04:39:53 +0200

    Add solution to Binary Search Tree

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,42 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor (data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert (data) {
+    if (this.data > data && this.left) {
+      this.left.insert(data);
+    }
+
+    if (this.data < data && this.right) {
+      this.right.insert(data);
+    }
+
+    if (this.data > data && !this.left) {
+      this.left = new Node(data);
+    }
+
+    if (this.data < data && !this.right) {
+      this.right = new Node(data);
+    }
+  }
+
+  contains (data) {
+    let currentNode = this;
+    while (currentNode) {
+      if (currentNode.data === data) return currentNode;
+
+      if (currentNode.data > data) currentNode = currentNode.left;
+      if (currentNode.data < data) currentNode = currentNode.right;
+    }
+
+    return null;
+  }
+}
 
 module.exports = Node;
