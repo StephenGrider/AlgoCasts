36d799766cd71921a1057ea23e15f410cceef0e2
bobbyvidal <bobbyvidal@gmail.com>
2019-12-22 11:03:04 -0600

    done

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -29,5 +29,26 @@ class Tree {
     constructor() {
       this.root = null;
     }
+    traverseBF(fn) {
+        const arr = [this.root];
+        while (arr.length) {
+          const node = arr.shift();
+    
+          arr.push(...node.children);
+          fn(node);
+        }
+      }
+    
+      traverseDF(fn) {
+        const arr = [this.root];
+        while (arr.length) {
+          const node = arr.shift();
+    
+          arr.unshift(...node.children);
+          fn(node);
+        }
+      }
+    }
+    
 
 module.exports = { Tree, Node };
