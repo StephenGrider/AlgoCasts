2c90da97413e5643ea9c30e38e021e2682c3a1bf
niesssiobhan <niesssiobhan@live.com>
2020-01-06 18:24:15 -0800

    working on solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,12 +17,17 @@
 //       '### '
 //       '####'
 
-const steps = n => {
-  
-};
+const steps = n => {};
 
 module.exports = steps;
 
 //--------NOTES------------//
 
-//remember that in each one of the steps that you have to inculde the spaces or the test will fail. For step three you will see that in each step there are 3 characters in each step even though some of them are spaces
+//remember that in each one of the steps that you have to inculde the spaces or the test will fail. For step 3 you will see that in each step there are 3 characters in each step even though some of them are spaces
+//you are going to want to iterate through the rows and the columns:
+//   _____________________________
+//  |_r0/c0_#_|_r0/c1___|_r0/c2___|
+//  |_r1/c0_#_|_r1/c1_#_|_r1/c2___|
+//  |_r2/c0_#_|_r2/c1_#_|_r2/c2_#_|
+
+// if the row and the column are the same then you will add in a '#'. If the column is less than the row then also add a '#'. Else add in a space to the stair
