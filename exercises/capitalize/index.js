20e62e5eae5c0bb612a971e3bdbdfa32f812668c
niesssiobhan <niesssiobhan@live.com>
2020-01-06 17:40:04 -0800

    first solution to capitalization

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,7 +8,13 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 const capitalize = str => {
-  
+  const words = [];
+
+  for (let word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  return words.join(" ");
 };
 
 module.exports = capitalize;
