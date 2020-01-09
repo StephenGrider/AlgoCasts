b3b051586763d82b0ec76a4046c01c0a59672eff
bobbyvidal <bobbyvidal@gmail.com>
2020-01-08 23:46:56 -0500

    finished area and circ of circle

---
+++ completed_exercises/area_circ_circle.js
@@ -0,0 +1,19 @@
+function area(r) {
+    
+    let rSquared = r*r
+
+    console.log(Math.PI * rSquared)
+
+}
+
+function circumference(r){
+
+    let rTimes2 = r*2
+
+    console.log(Math.PI * rTimes2)
+
+}
+
+area(4)
+
+circumference(4)
