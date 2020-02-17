d83013558d1434982ed98cae349d59b5d6a81946
Zermina Ejaz <zerminaejaz@gmail.com>
2020-02-17 18:10:43 -0500

    Added another way to reverseString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,8 +6,19 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-    return (str.split("")).reverse().join("")
+// function reverse(str) {
+//     return (str.split("")).reverse().join("")
+// }
+
+// another way
+function reverse(str){
+    let endpoint = str.length - 1
+    let reversed = ""
+    for(let i = endpoint; i > -1; i--)
+    {
+        reversed += str[i]
+    }
+    return reversed
 }
 
 module.exports = reverse;
