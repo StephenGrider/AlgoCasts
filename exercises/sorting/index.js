1cd8b206306db524ff558f073d30c70ee26d8fd4
Miss-Cheese <nat.galligan@gmail.com>
2020-04-13 16:11:01 -0400

    Selection sort done

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -16,7 +16,21 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let indexOfMin = i
 
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[indexOfMin]) {
+                indexOfMin = j
+            }
+        }
+        if (indexOfMin !== i) {
+            let lesser = arr[indexOfMin]
+            arr[indexOfMin] = arr[i]
+            arr[i] = lesser
+        }
+    }
+    return arr
 }
 
 function mergeSort(arr) {
