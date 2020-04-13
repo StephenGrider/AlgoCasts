1980bc3430be1d57a256571cd82a9ae549c953e0
Miss-Cheese <nat.galligan@gmail.com>
2020-04-13 13:55:23 -0400

    Bubble sort done

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,7 +2,17 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let stopIndex = arr.length - i - 1
+       for (let j = 0; j < stopIndex; j++) {
+         if (arr[j] > arr[j + 1]) {
+             let max = arr[j]
+             arr[j] = arr[j + 1]
+             arr[j + 1] = max
+         }
+       }
+    }
+    return arr
 }
 
 function selectionSort(arr) {
