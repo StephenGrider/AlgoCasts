54598a4c270eb3bf53a1e4c43a557dc44932faa4
denmercs <denmercs@gmail.com>
2020-04-25 02:12:19 -0500

    answers on reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split("").reduce((rev, char) => char + rev, "");
+}
+
+console.log(reverse("abcd"));
 
 module.exports = reverse;
+
+// SOLUTION 1
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// SOLUTION 2
+// function reverse(str) {
+//     let reversed = "";
+//     // not going to use the classic for loop
+//     for (let character of str) {
+//       reversed = character + reversed;
+//     }
+//     return reversed;
+//   }
