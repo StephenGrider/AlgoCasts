db14216a710efdd58ef00c6f28e94d87aa73b362
denmercs <denmercs@gmail.com>
2020-04-29 08:00:51 -0500

    vowels solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,21 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
+
+// solution 1
+// function vowels(str) {
+//     let count = 0;
+//     let checker = ["a", "e", "i", "o", "u"];
+//     for (let char of str.toLowerCase()) {
+//       if (checker.includes(char)) {
+//         count++;
+//       }
+//     }
+//     return count;
+//   }
