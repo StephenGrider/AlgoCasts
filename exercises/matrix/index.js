07953f40e007fa049848eb0baebfdc7cb0965060
denmercs <denmercs@gmail.com>
2020-05-01 15:42:20 -0500

    matrix solution

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,50 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let results = [];
+
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
+
+  let count = 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // top row
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = count;
+      count++;
+    }
+    startRow++;
+
+    // right column
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = count;
+      count++;
+    }
+    endColumn--;
+
+    // bottom row
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = count;
+      count++;
+    }
+    endRow--;
+
+    // left column
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = count;
+      count++;
+    }
+    startColumn++;
+  }
+
+  return results;
+}
 
 module.exports = matrix;
