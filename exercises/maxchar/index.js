315ee0e8b0ca5904651d919cdb768265449a5cb3
pipopotamasu <ec0156hx@gmail.com>
2020-05-06 17:49:11 +0900

    max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// function maxChar(str) {
+//   const charsMap = str.split('').reduce((map, char) => {
+//     map[char] = map[char] ? map[char] + 1 : 1
+//     return map
+//   }, {})
+
+
+//   const maxNumber = Math.max(...Object.values(charsMap))
+//   return Object.keys(charsMap).find((k) => charsMap[k] === maxNumber)
+// }
+
+function maxChar(str) {
+  const charsMap = str.split('').reduce((map, char) => {
+    map[char] = map[char] ? map[char] + 1 : 1
+    return map
+  }, {})
+
+  return Object.entries(charsMap).sort((a, b) => b[1] > a[1])[0][0]
+}
 
 module.exports = maxChar;
