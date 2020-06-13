1292684179e030ee117576ef5a49534de08cd415
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-06-13 18:35:51 -0500

    Reversing Integers
    Reversing integers,
    
    using parseFloat( ), reversing number input

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  return parseFloat(n.toString().split('').reverse().join('')) * Math.sign(n)
+}
+
+reverseInt(15)
 
 module.exports = reverseInt;
