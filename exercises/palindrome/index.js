c8aa12bc9fa69f77855248351d2328ee9f36ef63
An (Martin) Luo <aluo1.developer@gmail.com>
2020-07-11 09:24:45 +0800

    use two pointers to check palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,17 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left++] !== str[right--]) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = palindrome;
