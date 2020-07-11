acee111edf0e7b434d32600b173b4e357e09d41b
An (Martin) Luo <aluo1.developer@gmail.com>
2020-07-11 09:00:28 +0800

    use stack to reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,18 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  const stack = [];
+  for (let char of str) {
+    stack.push(char);
+  }
+
+  let reversedString = "";
+  while (stack.length) {
+    reversedString += stack.pop();
+  }
+
+  return reversedString;
+}
 
 module.exports = reverse;
