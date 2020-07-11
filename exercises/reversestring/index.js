0b4a84d8b87fdbc3cea7bb35ab3c29b3c846a02c
An (Martin) Luo <aluo1.developer@gmail.com>
2020-07-11 09:13:39 +0800

    provided solution for reversed string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,6 +7,16 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
+  return reverseUsingArrayHelper(str);
+}
+
+/**
+ * Reverse the string using stack.
+ *
+ * @param {*} str
+ * @returns
+ */
+function reverseMySolution(str) {
   const stack = [];
   for (let char of str) {
     stack.push(char);
@@ -18,6 +28,47 @@ function reverse(str) {
   }
 
   return reversedString;
+}
+
+/**
+ * Provided solution #1.
+ *
+ * @param {*} str
+ * @returns
+ */
+function reverseUsingReverse(str) {
+  return str.split("").reverse().join("");
+}
+
+/**
+ * Provided solution #2.
+ *
+ * This is similar to my solution, but more efficient: it requires only
+ * 1 for-loop, instead of 2.
+ *
+ * @param {*} str
+ * @returns
+ */
+function reverseUsingForLoop(str) {
+  let reversed = "";
+
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
+
+/**
+ * Provided solution #3.
+ *
+ * @param {*} str
+ * @returns
+ */
+function reverseUsingArrayHelper(str) {
+  return str
+    .split("")
+    .reduce((reversed, current) => `${current}${reversed}`, "");
 }
 
 module.exports = reverse;
