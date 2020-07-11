ea7353964498080a0586a44a323a09fcc7f3b84b
An (Martin) Luo <aluo1.developer@gmail.com>
2020-07-11 09:35:12 +0800

    add 2 provided solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,6 +8,16 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
+  return palindromeMySolution(str);
+}
+
+/**
+ * My solution using two pointers.
+ *
+ * @param {*} str
+ * @returns
+ */
+function palindromeMySolution(str) {
   let left = 0;
   let right = str.length - 1;
 
@@ -20,4 +30,27 @@ function palindrome(str) {
   return true;
 }
 
+/**
+ * Provided most straightforward solution.
+ *
+ * @param {*} str
+ * @returns
+ */
+function palindromeProvidedSolution(str) {
+  const reversed = str.split("").reverse().join("");
+
+  return reversed === str;
+}
+
+/**
+ * Provided solution #2.
+ *
+ * This is still not an idea solution, as we should only need to check
+ * half of the entire string, while this solution check the entire one.
+ *
+ * @param {*} str
+ */
+function palindromeUsingEvery(str) {
+  str.split("").every((char, i) => char === str[str.length - i - 1]);
+}
 module.exports = palindrome;
