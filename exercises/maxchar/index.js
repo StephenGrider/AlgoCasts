f5513e5dd2ba4b68339e72a263c470768d5cabd4
An (Martin) Luo <aluo1.developer@gmail.com>
2020-07-13 09:45:39 +0800

    use map to count the freq

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const freq = new Map();
+  let mostOccurrence = 0;
+  let mostOccurredChar = "";
+
+  for (let char of str) {
+    let occurrence = 1;
+
+    if (freq.has(char)) {
+      occurrence += freq.get(char);
+      freq.set(char, occurrence);
+    } else {
+      freq.set(char, occurrence);
+    }
+
+    if (occurrence > mostOccurrence) {
+      mostOccurrence = occurrence;
+      mostOccurredChar = char;
+    }
+  }
+
+  return mostOccurredChar;
+}
 
 module.exports = maxChar;
