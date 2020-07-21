3d451187e66acbd6a288fc8ec91b25e8f30fee60
An (Martin) Luo <aluo1.developer@gmail.com>
2020-07-21 09:30:12 +0800

    add provided regex solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,6 +8,17 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
+  return vowelsRegex(str);
+}
+
+/**
+ * This is the solution I wrote, and it's the 1st provided
+ * solution as well.
+ *
+ * @param {*} str
+ * @returns
+ */
+function vowelsMySolution(str) {
   const vowels = ["a", "e", "i", "o", "u"];
 
   let count = 0;
@@ -18,6 +29,17 @@ function vowels(str) {
   }
 
   return count;
+}
+
+/**
+ * Use regular expression to count the vowels.
+ *
+ * @param {*} str
+ * @returns
+ */
+function vowelsRegex(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
