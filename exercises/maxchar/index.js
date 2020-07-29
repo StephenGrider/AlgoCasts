35245fb98c71189d43b76bd3818bc8a117b4d98d
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-07-29 17:54:45 -0500

    Setting Up Object
    maxChar method impemented.
    
    we can follow these steps below in order to return the most commonly used character in a string.
    
    1. We need to create an object that will hold the amount of times each character shows up. We do this by defining an empty object, and looping through our string input.
    
    2. While we loop through our string, we can check if there is anything in our object first with an IF statement
    
    3. After checking our object if its empty or not, we can assign our object some values now.

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // store strings into an object
+  // string_object = {}
+  // // iterate through string to get most common character
+  // for (let char of str) {
+  //   string_object[char] = string_object[char] + 1 || 1
+  // }
+  // // output most common used character
+
+  const charMap = {}
+
+  for (let i of str) {
+    if (charMap[i]){
+      charMap[i] + 1
+    } else {
+      charMap[i] = 1
+    }
+  }
+
+  console.log(charMap)
+
+
+
+}
 
 module.exports = maxChar;
--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,21 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+
+// Solution 1
+// function palindrome(str) {
+//
+//   const reversed = str.split('').reverse().join('')
+//   return str === reversed
+//
+// }
+
+
+//Solution 2
+function palindrome(str){
+  return str.split('').every((char, i)=> {
+    return char === str[str.length - i - 1]
+  })
+}
 
 module.exports = palindrome;
