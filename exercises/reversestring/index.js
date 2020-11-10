d2cdad02788396309232c2f6b52bc4cb457fe198
Uzias Rivera <uziasrivera@gmail.com>
2020-11-09 19:23:23 -0800

    complete

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((acc, curr) => curr + acc, '')
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//     var reversedStr = ""
+//     for(let i=str.length; i>0; i--){
+//         reversedStr += str[i-1]
+//     }
+//     return reversedStr
+// }
+
+
+// function reverse(str) {
+//     var reversedStr = ""
+//     for(let character of str){
+//         reversedStr = character + reversedStr
+//     }
+//     return reversedStr
+// }
