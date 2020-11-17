84354ef239568511037c2a4d4124e251cd02a42e
VindasJohan <vindasjohan@gmail.com>
2020-11-17 14:05:03 -0600

    'ReverseString'

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,49 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//     str = str.split('');
+//     let rvStr='';
+//     for(let i = str.length -1; i >= 0; i--){
+//         rvStr += str[i];
+//     }
+//     return rvStr;
+// }
+
+// function reverse(str) {
+//     const arr = str.split('');
+//     arr.reverse();
+//     return arr.join('');
+// }
+
+// function reverse(str) {
+//     return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+// //Solution #2
+function reverse(str){
+    let reversed = '';
+    for(let character of str){
+        reversed = character + reversed;
+        debugger;
+    }
+    return reversed;
+}
+
+//solution #3
+// function reverse(str){
+//     //return str.split('').reduce((rev, char)=> char + rev ,'');
+//     debugger;
+//     return str.split('').reduce((rev, char)=> {
+//         return char + rev;
+//     } ,'');
+// }
+//reverse('asdf');
 
 module.exports = reverse;
+// ctrl + C to Exit Test.
+// $ node inspect index.js
+// $ node --inspect-brk app.js
