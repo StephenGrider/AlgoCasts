855dfbdb85e24d6718e4bffd3a074a8a9579cc9b
github username <jenniferagrenier@gmail.com>
2020-12-01 16:34:34 -0700

    3 solutions for reverse a string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,11 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-  return str
-    .split('')
-    .reverse()
-    .join('');
+// function reverse(str) {
+//     return str
+//         .split('')
+//         .reverse()
+//         .join('');
+// }
+// function reverse(str){
+//     let reversed = '';
+
+//     for (let character of str){
+//         reversed = character + reversed;
+//     }
+//     return reversed; 
+// }
+
+function reverse (str){
+    return str.split('').reduce((rev, char) =>  char + rev
+    , '');
 }
 
 module.exports = reverse;
