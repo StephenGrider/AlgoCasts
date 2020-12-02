ba2a3ac2c69cb5554a287e4454053e8b25673d62
github username <jenniferagrenier@gmail.com>
2020-12-02 13:16:34 -0700

    first solve for reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,15 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
-
+function reverseInt(n) {
+    return(
+        parseFloat(
+            n 
+                .toString()
+                .split('')
+                .reverse()
+                .join('')
+       ) *Math.sign(n)
+    )
+}
 module.exports = reverseInt;
