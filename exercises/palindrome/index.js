bb86222309ad5913be12f07df8bc2f4124a73bc4
github username <jenniferagrenier@gmail.com>
2020-12-02 12:51:50 -0700

    second and third solve for palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,14 +7,25 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {
-    let rev = str.split('').reduce((rev, char) => char + rev
-    ,'');
-    if(rev === str){
-        return true
-    } else {
-        return false
-    }
+// function palindrome(str) {
+//     let rev = str.split('').reduce((rev, char) => char + rev
+//     ,'');
+//     if(rev === str){
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// function palindrome (str){
+//     const reversed = str.split('').reverse().join('');
+//     return str === reversed;
+// }
+
+function palindrome (str){
+    return str.split('').every( (char, i) => {
+        return char === str[str.length - i - 1];
+    });
 }
 
 module.exports = palindrome;
