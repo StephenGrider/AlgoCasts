d231220c8c1cbc4e6589f80b8c1ebfa84d528467
“Luis <l.garcia24@hotmail.com>
2021-01-14 23:37:40 -0600

    passed second problem

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,15 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    reversed = str.split("").reverse().join("")
+
+    if (reversed === str) {
+        return true
+    } else {
+        return false
+    }
+
+}
 
 module.exports = palindrome;
