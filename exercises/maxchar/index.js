62206deaf53ff82448b8bf12fa96098a3ca3950d
“Luis <l.garcia24@hotmail.com>
2021-01-16 23:35:38 -0600

    finished 4th problem (finding max character in string)

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const character = {}
+    let max = 0
+    let maxChar = ''
+
+
+    for (let i = 0; i < str.length; i++) {
+        if (!character[str[i]]) {
+            character[str[i]] = 1
+        } else {
+            character[str[i]] += 1
+        }
+    }
+
+    for (let char in character) {
+        if (character[char] > max) {
+            max = character[max]
+            maxChar = char
+        }
+    }
+
+    return maxChar
+}
 
 module.exports = maxChar;
