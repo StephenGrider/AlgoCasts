2fd568be9cf53d5aec27b6f481cb74fecc8f6bbb
Dragan Cajic <cajic_dragan@yahoo.com>
2021-03-19 23:50:32 +0100

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,21 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+	reversed = '';
+
+	for (let i = 0; i < str.length; i++) {
+		reversed += (str[str.length - 1 - i]);
+	}
+
+	//console.log(reversed);
+	return reversed;
+}
+
+console.log(reverse('Greetings!'));
 
 module.exports = reverse;
