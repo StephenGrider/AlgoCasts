6a3c8e39212bf555547f9de420ded02c55e917ca
Robby Singh <kingraj2323@gmail.com>
2021-05-09 16:56:44 -0500

    Used reduce method for my solution #3

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,17 +7,26 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  let reversed = '';
-
-  for (let character of str) {
-    reversed = character + reversed;
-  }
-
-  return reversed;
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, '')
 }
 
 module.exports = reverse;
 
+// Solution #1
 // function reverse(str) {
 //     return str.split('').reverse().join('');
 //   }
+
+
+// Solution #2
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
