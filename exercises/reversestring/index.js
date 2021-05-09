69ae7d3adff229f93293fd898e79cb21b22f0852
Robby Singh <kingraj2323@gmail.com>
2021-05-09 00:31:30 -0500

    my first solution for this exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,10 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  const arr = str.split('');
+  arr.reverse();
+  return arr.join('');
+}
 
 module.exports = reverse;
