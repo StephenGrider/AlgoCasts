bd53f5b275da541b7a0c287240c1cdb9705376c2
Robby Singh <kingraj2323@gmail.com>
2021-05-09 00:37:47 -0500

    Made my solution code even shorter/simplified

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,9 +7,7 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  const arr = str.split('');
-  arr.reverse();
-  return arr.join('');
+  return str.split('').reverse().join('');
 }
 
 module.exports = reverse;
