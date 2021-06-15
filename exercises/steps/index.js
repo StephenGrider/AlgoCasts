1833e2c257151225521480d0257e7fdc133205a8
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-06-15 15:17:38 -0400

    first solution started recurrsion approach

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -19,13 +19,31 @@
 
 function steps(n) {
     // from 0 to n(iterate through rows)
+    for (let row = 0; row < n; row++) {
+
         // create an empty string, 'stair'
+        let stair = '';
+
         // from 0 to n (iterate through columns)
+        for (let column = 0; column < n; column++) {
+            
             // if the current column is equal to or less than the current row
+            if (column <= row) {
+
                 // add a "#' to 'stair'
-            // else
+                stair += '#';
+            
+                // else
+            } else {
+
                 // add a '#' to 'stair'
+                stair += ' ';
+            }
+        }
+
         // console.log 'stair'
+        console.log(stair);
+    }
 }
 
 module.exports = steps;
