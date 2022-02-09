6ed332e7c4dff4a7c9267127bedf0615b4795411
d-ahn10 <no-reply.email.address>
2022-02-09 13:56:45 -0800

    good-to-best 3 solutions for reverse a string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    //ES6 Best
+    return str.split('').reduce((reversed, char) => char + reversed, '');
+
+}
 
 module.exports = reverse;
+
+    //old syntax
+    // let reversedWord = '';
+    // for (let i = str.length - 1; i >= 0; i--) {
+    //     let eachChar = str[i];
+    //     reversedWord += eachChar;
+    // }
+    // return reversedWord;
+
+    //ES6 better
+    // let reversedWord = '';
+    // for (let char of str) {
+    //     reversedWord = char + reversedWord; 
+    // }
+    // return reversedWord;
