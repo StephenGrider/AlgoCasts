81fc825e166944a31118c0fe55ad341a585089a6
Olha Gruzglina <olga.zhmura@gmail.com>
2022-05-06 15:53:56 -0400

    reversestring solution using arr.reverse()

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,18 +7,26 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    if (str.length === 1) {
-        return str;
-    }
+    // if (str.length === 1) {
+    //     return str;
+    // }
      
-    let reversedStr = '';
+    // let reversedStr = '';
 
-    for (let i = str.length-1; i >= 0; i--) {
-        reversedStr += str[i];
-        console.log(reversedStr);
-    }
-    return reversedStr;
+    // for (let i = str.length-1; i >= 0; i--) {
+    //     reversedStr += str[i];
+    //     console.log(reversedStr);
+    // }
+    //return reversedStr;
+
+    // using reverse for the array
+
+    // const arr = str.split('');
+    // arr.reverse();
+    // return reversedStr = arr.join('');
+    // or in one line:
+    return str.split('').reverse().join('');  
 }
 
-//module.exports = reverse('Greetings!'); - if you want to run only this file
+//module.exports = reverse('Greetings!'); //- if you want to run only this file
 module.exports = reverse;
