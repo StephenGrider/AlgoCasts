6c9bd18ede47ee41403001eb82533de19c0f33ca
Alison Childers <alisonchilders314@gmail.com>
2022-06-22 15:02:34 -0500

    Solved Reverse String exercise (plus added other solutions from instructor)

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    debugger;
+    return str.split('').reverse().join('');
+}
+
+reverse('asdf');
 
 module.exports = reverse;
+
+// Solution #1
+// function reverse(str) {
+//     let myArr = str.split('');
+//     let reversed = myArr.reverse();
+//     let newStr = reversed.join('');
+//     return newStr;
+// }
+// cleaned up and made it into the solution above
+
+// Solution #2
+// function reverse(str) {
+// let reversed = "";
+// 
+// for (let character of str) {
+//    reversed = character + reversed;
+//}
+// return reversed;
+// }
+
+// function reverse(str) {
+//     return str.split('').reduce((reversed, character) =>
+//         character + reversed , '');
+// }
