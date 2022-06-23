2d39f3e8e8a90e15a1a51bc1254384369cec2e35
Alison Childers <alisonchilders314@gmail.com>
2022-06-23 08:32:40 -0500

    Solved Reverse Integer Problem

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,15 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let numArr = n.toString().split('');
+
+    if (numArr[0] == '-') {
+        numArr.shift();
+        return parseInt(numArr.reverse().join('')) * -1;
+    }
+
+    return parseInt(numArr.reverse().join(''));
+}
 
 module.exports = reverseInt;
