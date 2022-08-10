96b16ea36ad8505c1712ba36c2c008b755af8f95
Angelique Abacajan <ma_utsey@yahoo.com>
2022-08-10 09:51:12 -0400

    Implemented Reverse String Solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,8 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split("").reduce((rev, char) => char + rev, "");
+}
 
 module.exports = reverse;
