d2d68d94868368f66f6569cff0d6e4a5ba3ff420
hirooutdoor <hmaeyama0510@gmail.com>
2022-11-01 09:33:37 +0900

    feat: possible solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,18 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    if (Math.sign(n) === 1) {
+        if (n.toString().split("")[n.length - 1] === 0) {
+            return parseInt(n.toString().split("").slice(-1).reverse().join(""))
+        }
+        return parseInt(n.toString().split("").reverse().join(""))
+    } else {
+        if (n === 0) {
+            return - parseInt("-" +(-n).toString().split("").reverse().join(""))
+        }
+        return  parseInt("-" +(-n).toString().split("").reverse().join(""))
+    }
+}
 
 module.exports = reverseInt;
