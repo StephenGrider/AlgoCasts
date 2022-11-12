34576e781d2cae1d50442576fa4ef34cd99e7cab
MikeAriyo <ariyoadekola2@gmail.com>
2022-11-12 09:36:41 +0100

    possible solutions for string reversal

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split("").reduce((rev, char) => char + rev, "");
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//     return str.split("").reverse().join("");
+
+// split turns the string into an array, reversed reverses the items in the array, Join converts the array back into a string.
+// }
+
+// function reverse(str) {
+//     let reversed = "";
+//     for (let character of str) {
+//       reversed = character + reversed;
+//     }
+//     return reversed;
+// declare an empty string variable "reversed", use a for loop to loop through the characters of the string, add it to the empty string.
+
+// question : does a for loop store the last iterated item first?
+// }
