ba7d93a88ec0d16ff8b0edd280750e3738b592fb
Ryley Hynes <ryleyhynes@gmail.com>
2022-11-18 16:12:42 -0600

    first 6 problems completed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,36 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//********ANSWER 1********
+//split turns it into an array
+
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+
+
+
+//********ANSWER 2********
+
+// function reverse(str) {
+//     let reversed = '';
+    
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+//     return reversed;
+// }
+
+
+
+//********ANSWER 3********
+/*reduce array helper is used to take all the different values in an array and 
+condense them all down to one singular value. The empty string is the second optional starting argument*/
+// str = ['a', 'p', 'p', 'l', 'e']
+function reverse(str) {
+    
+    return str.split('').reduce((reversed, character) => character + reversed, '');
+}
 
 module.exports = reverse;
