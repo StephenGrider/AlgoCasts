c5ca7d400de7d54f7bab39c07b6015a20cbe3a99
MikeAriyo <ariyoadekola2@gmail.com>
2023-01-10 18:08:39 +0100

    added comments to solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -11,6 +11,12 @@
 function reverseInt(n) {
   const reversed = n.toString().split("").reverse().join("");
   return parseInt(reversed) * Math.sign(n);
+
+  // convert the interger to a string first by using the the toString() array method
+  // convvert the string to an array by using the split('') method
+  // reverse the elements of the array and jpoin them back to a string
+  //parseInt(n) takes in the reversed string and turns it to an integer
+  // Math.sign() helps us with the sign of the integer
 }
 
 module.exports = reverseInt;
