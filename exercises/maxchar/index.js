091589543ad03406cd00855a7431313655c7141e
MikeAriyo <ariyoadekola2@gmail.com>
2023-02-10 18:01:30 +0100

    problem trick

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,9 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+// other notes
+//Trick - take the strings and essentially convert it to an object`
+
 function maxChar(str) {}
 
 module.exports = maxChar;
