60c465d347da872e7bbe1872508b499401892e55
MikeAriyo <ariyoadekola2@gmail.com>
2023-02-15 10:42:55 +0100

    final solution

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -13,11 +13,15 @@
 //   buzz
 
 function fizzBuzz(n) {
-  for (let i = 0; i <= n; i++) {
-    if (i % 3 === 0) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
       console.log("fizz");
     } else if (i % 5 === 0) {
       console.log("buzz");
+    } else {
+      console.log(i);
     }
   }
 }
