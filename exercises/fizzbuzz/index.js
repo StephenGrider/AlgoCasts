ce96d7270447da5bb1173211776057c3b5361778
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-07-18 16:00:20 -0400

    Feat & Test: FizzBuzz
    Add solution to fizzBuzz challenge. Add tests to check if fizzBuzz outputs the correct results.

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,21 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  let str = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      str.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      str.push("Fizz");
+    } else if (i % 5 === 0) {
+      str.push("Buzz");
+    } else {
+      str.push(i.toString());
+    }
+  }
+
+  return str;
+}
 
 module.exports = fizzBuzz;
--- exercises/fizzbuzz/test.js
+++ exercises/fizzbuzz/test.js
@@ -1,37 +1,39 @@
-const fizzBuzz = require('./index');
+const fizzBuzz = require("./index");
 
-test('fizzBuzz function is defined', () => {
+test("fizzBuzz function is defined", () => {
   expect(fizzBuzz).toBeDefined();
 });
 
-test('Calling fizzbuzz with `5` prints out 5 statements', () => {
-  fizzBuzz(5);
-
-  expect(console.log.mock.calls.length).toEqual(5);
+test("Calling fizzbuzz with `3` returns an array of 5 correct values", () => {
+  expect(fizzBuzz(3)).toEqual(["1", "2", "Fizz"]);
 });
 
-test('Calling fizzbuzz with 15 prints out the correct values', () => {
-  fizzBuzz(15);
+test("Calling fizzbuzz with `5` returns an array of 5 correct values", () => {
+  expect(fizzBuzz(5)).toEqual(["1", "2", "Fizz", "4", "Buzz"]);
+});
 
-  expect(console.log.mock.calls[0][0]).toEqual(1);
-  expect(console.log.mock.calls[1][0]).toEqual(2);
-  expect(console.log.mock.calls[2][0]).toEqual('fizz');
-  expect(console.log.mock.calls[3][0]).toEqual(4);
-  expect(console.log.mock.calls[4][0]).toEqual('buzz');
-  expect(console.log.mock.calls[5][0]).toEqual('fizz');
-  expect(console.log.mock.calls[6][0]).toEqual(7);
-  expect(console.log.mock.calls[7][0]).toEqual(8);
-  expect(console.log.mock.calls[8][0]).toEqual('fizz');
-  expect(console.log.mock.calls[9][0]).toEqual('buzz');
-  expect(console.log.mock.calls[10][0]).toEqual(11);
-  expect(console.log.mock.calls[11][0]).toEqual('fizz');
-  expect(console.log.mock.calls[12][0]).toEqual(13);
-  expect(console.log.mock.calls[13][0]).toEqual(14);
-  expect(console.log.mock.calls[14][0]).toEqual('fizzbuzz');
+test("Calling fizzbuzz with 15 returns the correct values", () => {
+  expect(fizzBuzz(15)).toEqual([
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz",
+  ]);
 });
 
 beforeEach(() => {
-  jest.spyOn(console, 'log');
+  jest.spyOn(console, "log");
 });
 
 afterEach(() => {
