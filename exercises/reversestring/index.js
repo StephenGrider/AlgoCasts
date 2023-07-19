f30c1b6a11d5f493c6ed52d3ba296bd329f5444b
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-07-19 13:31:43 -0400

    feat & test: reversestring
    Add solution to return a reverse string.
    
    Test if returned string is reversed as expected.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // #1 Simple solution
+  //   let strArr = str.split("").reverse().join("");
+  // #2 Solution without relying on reverse method
+  // Does not work on leet code because it wants the original array mutated instead of returning a new reversed array
+  //   let strArr = [];
+  //   str.split("").forEach((char) => strArr.unshift(char));
+  //   return strArr.join("");
+  //#3 solution without methods. Again, not going to work on leet code.
+  //   let reversedStr = "";
+  //   for (let char of str) {
+  //     reversedStr = char + reversedStr;
+  //   }
+  //   return reversedStr;
+  // #4 Using reduce method, again won't work on leet code.
+  return str.split("").reduce((rev, char) => char + rev, "");
+}
+
+reverse("asdf");
 
 module.exports = reverse;
