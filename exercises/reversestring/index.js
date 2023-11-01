63e4fbd3a00b9cd545437b58b9ff7fa3d15e2910
Xu <whitenight.xiaxu@gmail.com>
2023-11-01 11:26:04 +0800

    solution2 reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,7 +7,13 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    return str.split('').reverse().join('')
+    let reversed = ''
+
+    for (let character of str) {
+        reversed = character + reversed
+    }
+    return reversed
+
 }
 
 module.exports = reverse;
