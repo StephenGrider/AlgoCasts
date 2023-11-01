834d495d70335817289a5a88fd200c24b6c361d2
Xu <whitenight.xiaxu@gmail.com>
2023-11-01 11:09:07 +0800

    solution1 reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,8 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reverse().join('')
+}
 
 module.exports = reverse;
