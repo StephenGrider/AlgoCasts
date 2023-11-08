3224337074439a040ed6855ab92c0f61d6cadc9d
Xu <whitenight.xiaxu@gmail.com>
2023-11-08 11:17:07 +0800

    max char solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+   const charMap = {}
+    for (const char of str) {
+        charMap[char] = charMap[char]  + 1 || 1
+    }
+    let max = 0
+    let maxChar = ''
+    for (const char in charMap) {
+        if(charMap[char] > max) {
+            max = charMap[char]
+            maxChar = char
+        }
+    }
+    return maxChar
+}
 
 module.exports = maxChar;
