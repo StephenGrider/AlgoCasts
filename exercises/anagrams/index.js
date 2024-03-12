33c2a60ee1d99e9ae7774e70cfe287a895bf7dc6
Tyler Furgalack <104029862+tylerfurgalack@users.noreply.github.com>
2024-03-12 15:07:54 -0400

    section 9

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,65 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
 
 module.exports = anagrams;
+
+// first solution
+// function anagrams(stringA, stringB) {
+//     const aCharMap = buildCharMap(stringA);
+//     const bCharMap = buildCharMap(stringB);
+
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//       return false;
+//     }
+
+//     for (let char in aCharMap) {
+//       if (aCharMap[char] !== bCharMap[char]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+
+//   function buildCharMap(str) {
+//     const charMap = {};
+//     for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//       charMap[char] = charMap[char] + 1 || 1;
+//     }
+
+//     return charMap;
+//   }
+
+//my half of an attempt
+// function anagrams(stringA, stringB) {
+//     const stringAMap = {}
+//     const stringBMap ={}
+//     stringA = stringA.replace(/[^\w]/g, "").toLowerCase();
+//     stringB = stringB.replace(/[^\w]/g, "").toLowerCase();
+//     if(stringA.length != stringB.length){
+//         return false
+//     } else {
+//         for(char of stringA){
+//             if(charAMap[char]){
+//                 charAMap[char]++
+//             } else {
+//                 charAMap[char] = 1
+//             }
+//         }
+//         for(char of stringB){
+//             if(charBMap[char]){
+//                 charBMap[char]++
+//             } else {
+//                 charBMap[char] = 1
+//             }
+//         }
+
+//     }
+// }
